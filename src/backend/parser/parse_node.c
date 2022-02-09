419da9ccfa665ff3a20506a9d3a2299c389f35b6
Sharu Goel <30777678+thephantomthief@users.noreply.github.com>
2022-02-09 18:26:58 +0530

    Preserve the case of column names in SELECT relation.*
    When returning results in a SELECT relation.* statement in T-SQL, the
    column names should preserve the case they were created in to mimic
    T-SQL behavior. We now find the original case in the pg_attributes
    table.
    
    Task: BABEL-2846
    Author: Rishabh Tanwar <ritanwar@amazon.com>
    Signed-off-by: Sharu Goel <goelshar@amazon.com>

--- src/backend/parser/parse_node.c
+++ src/backend/parser/parse_node.c
@@ -65,6 +65,7 @@ make_parsestate(ParseState *parentParseState)
 		/* all hooks are copied from parent */
 		pstate->p_pre_columnref_hook = parentParseState->p_pre_columnref_hook;
 		pstate->p_post_columnref_hook = parentParseState->p_post_columnref_hook;
+		pstate->p_post_expand_star_hook = parentParseState->p_post_expand_star_hook;
 		pstate->p_paramref_hook = parentParseState->p_paramref_hook;
 		pstate->p_coerce_param_hook = parentParseState->p_coerce_param_hook;
 		pstate->p_ref_hook_state = parentParseState->p_ref_hook_state;
