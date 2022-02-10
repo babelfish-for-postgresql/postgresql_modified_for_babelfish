a58e2d99eec2f2d9665c6e706f2d3f79d0d7f8e6
Dipesh Dhameliya <dipeshdhameliya125@gmail.com>
2022-02-10 20:29:17 +0530

    Crash in exec_stmt_iterative due to trigger with error handling
    Root cause: The problem was due to composite trigger with error
    handling. When we run the DML, we record trigger events separately for
    T-SQL triggers.
    
    AfterTriggersTableData is recorded in composite triggers but a reference
    is also maintained by PostgreSQL triggers. When we cleanup PostgreSQL
    trigger data (due to rollback to savepoint etc.), we skip cleanup of
    AfterTriggersTableData if composite triggers are active. Later,
    composite triggers delete the AfterTriggersTableData.
    
    However, if there is an error during DML execution, we first remove
    composite triggers (which deletes AfterTriggersTableData) and later do
    rollback to savepoint. In this case, the PostgreSQL trigger will have a
    stale reference to AfterTriggersTableData since it is already deleted by
    composite trigger and will lead to segment fault issues.
    
    Solution:
    In the event of error, EndCompositeTriggers() is being invoked before
    AfterTriggerEndSubXact() which will cleanup
    AfterTriggersTableData. But reference to the same is maintained by PG
    trigger which in turn would have a stale reference. So, setting
    reference to AfterTriggersTableData to the NIL explicitly if query_depth
    of
    PG trigger is more than query_depth of the composite trigger.
    
    Task: BABEL-2845
    Author: Dipesh Dhameliya dddhamel@amazon.com
    Signed-off-by: Dipesh Dhameliya dddhamel@amazon.com

--- src/backend/commands/trigger.c
+++ src/backend/commands/trigger.c
@@ -3742,6 +3742,7 @@ typedef struct CompositeTriggerData
 	int triggerLevel;
 	uint32 xactSubxid;
 	bool cleanupReq;
+	int query_depth;
 	struct CompositeTriggerData *next;
 } CompositeTriggerData;
 
@@ -6255,6 +6256,7 @@ void AddCompositeTriggerLevelData(void)
 		triggers->next = compositeTriggers.triggers;
 		triggers->xactSubxid = GetCurrentSubTransactionId();
 		triggers->cleanupReq = true;
+		triggers->query_depth = afterTriggers.query_depth;
 		compositeTriggers.triggers = triggers;
 	}
 }
@@ -6311,6 +6313,18 @@ void EndCompositeTriggers(bool error)
 		}
 		compositeTriggers.triggers = triggers->next;
 		AfterTriggerFreeQuery(&triggers->data, true);
+
+		/*
+		 * In case of error, EndCompositeTriggers() is being invoked before 
+		 * AfterTriggerEndSubXact(). So, This will cleanup AfterTriggersTableData 
+		 * recorded in compositeTriggers. But reference to the AfterTriggersTableData 
+		 * is also maintained by PG trigger which in turn would have stale reference.
+		 * So, setting reference to AfterTriggersTableData to the NIL explicitly here 
+		 * to avoid seg fault issues.
+		 */ 
+		if (afterTriggers.query_depth < afterTriggers.maxquerydepth && afterTriggers.query_depth >= triggers->query_depth)
+			afterTriggers.query_stack[afterTriggers.query_depth].tables = NIL;
+
 		pfree(triggers);
 	}
 	compositeTriggers.triggerLevel--;
