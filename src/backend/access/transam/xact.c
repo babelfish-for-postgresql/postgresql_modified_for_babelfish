19d678678a84131e47791609fad81ebc801bd177
Robert Haas <rhaas@postgresql.org>
2022-03-24 14:36:06 -0400

    Fix possible recovery trouble if TRUNCATE overlaps a checkpoint.
    If TRUNCATE causes some buffers to be invalidated and thus the
    checkpoint does not flush them, TRUNCATE must also ensure that the
    corresponding files are truncated on disk. Otherwise, a replay
    from the checkpoint might find that the buffers exist but have
    the wrong contents, which may cause replay to fail.
    
    Report by Teja Mupparti. Patch by Kyotaro Horiguchi, per a design
    suggestion from Heikki Linnakangas, with some changes to the
    comments by me. Review of this and a prior patch that approached
    the issue differently by Heikki Linnakangas, Andres Freund, Álvaro
    Herrera, Masahiko Sawada, and Tom Lane.
    
    Discussion: http://postgr.es/m/BYAPR06MB6373BF50B469CA393C614257ABF00@BYAPR06MB6373.namprd06.prod.outlook.com

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -1315,8 +1315,9 @@ RecordTransactionCommit(void)
 		 * This makes checkpoint's determination of which xacts are delayChkpt
 		 * a bit fuzzy, but it doesn't matter.
 		 */
+		Assert((MyProc->delayChkpt & DELAY_CHKPT_START) == 0);
 		START_CRIT_SECTION();
-		MyProc->delayChkpt = true;
+		MyProc->delayChkpt |= DELAY_CHKPT_START;
 
 		SetCurrentTransactionStopTimestamp();
 
@@ -1417,7 +1418,7 @@ RecordTransactionCommit(void)
 	 */
 	if (markXidCommitted)
 	{
-		MyProc->delayChkpt = false;
+		MyProc->delayChkpt &= ~DELAY_CHKPT_START;
 		END_CRIT_SECTION();
 	}
 
