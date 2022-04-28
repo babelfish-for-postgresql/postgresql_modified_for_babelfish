hanzoai/sql

Fix silly mistake in Assert

src/backend/access/heap/heapam.c
@@ -5241,7 +5241,8 @@ failed:
 		 * this assert is slightly different from the equivalent one in
 		 * heap_delete and heap_update.
 		 */
-		Assert(HeapTupleWouldBlock || !(tuple->t_data->t_infomask & HEAP_XMAX_INVALID));
+		Assert((result == HeapTupleWouldBlock) ||
+			   !(tuple->t_data->t_infomask & HEAP_XMAX_INVALID));
 		hufd->ctid = tuple->t_data->t_ctid;
 		hufd->xmax = HeapTupleHeaderGetUpdateXid(tuple->t_data);
 		if (result == HeapTupleSelfUpdated)

babelfish-for-postgresql/postgresql_modified_for_babelfish

src/backend/access/heap/heapam.c
@@ -4506,7 +4506,8 @@ failed:
 		 * this assert is slightly different from the equivalent one in
 		 * heap_delete and heap_update.
 		 */
-		Assert(TM_WouldBlock || !(tuple->t_data->t_infomask & HEAP_XMAX_INVALID));
+		Assert((result == TM_WouldBlock) ||
+			   !(tuple->t_data->t_infomask & HEAP_XMAX_INVALID));
 		Assert(result != TM_Updated ||
 			   !ItemPointerEquals(&tuple->t_self, &tuple->t_data->t_ctid));
 		tmfd->ctid = tuple->t_data->t_ctid;