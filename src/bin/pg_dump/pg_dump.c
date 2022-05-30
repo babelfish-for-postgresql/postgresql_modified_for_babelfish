61eea1e7ec4cf3da539480c745b890d9082a6c4f
jungkook <2jungkook@users.noreply.github.com>
2022-05-30 14:45:10 -0700

    BABEL: Fix + and / operators during pg_upgrade (#14)
    [Engine]
    
    In Babelfish v1.2, we had to re-define some operators to fix issues.
    However, we were not able to apply the fix in the upgrade script because
    customer-defined object may have depended on those operators.
    
    This commit introduces a fix by doing it during pg_upgrade.
    
    [Extension]
    
    As this issue can only be tested when a user initiates Major Version
    Upgrade from Babelfish v1.0 or v1.1, added a temporary test case
    manipulating pg_operator directly. Once we have a Github action that can
    test MVU from v1.0, we should remove this test case to avoid
    side-effects to other test cases.
    
    Task: BABEL-3112
    Signed-off-by: Jungkook Lee jungkook@amazon.com
    (cherry picked from commit af50ae2adaeda6d5078190b8007cd93f2c8609a0)

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -12288,6 +12288,9 @@ dumpOpr(Archive *fout, const OprInfo *oprinfo)
 	oprregproc = convertRegProcReference(oprcode);
 	if (oprregproc)
 	{
+		/* Special handling for Babelfish */
+		fixOprRegProc(fout, oprinfo, oprleft, oprright, &oprregproc);
+
 		appendPQExpBuffer(details, "    FUNCTION = %s", oprregproc);
 		free(oprregproc);
 	}
