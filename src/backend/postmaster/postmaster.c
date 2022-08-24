e9f6b72c359486cf96e5af975d6df1e98f63da3d
Michael Paquier <michael@paquier.xyz>
2022-08-24 12:57:13 +0900

    Allow parallel workers to retrieve some data from Port
    BABELFISH-CONFLICT: see Postgres community repo for original commit
    
    This commit moves authn_id into a new global structure called
    ClientConnectionInfo (mapping to a MyClientConnectionInfo for each
    backend) which is intended to hold all the client information that
    should be shared between the backend and any of its parallel workers,
    access for extensions and triggers being the primary use case.  There is
    no need to push all the data of Port to the workers, and authn_id is
    quite a generic concept so using a separate structure provides the best
    balance (the name of the structure has been suggested by Robert Haas).
    
    While on it, and per discussion as this would be useful for a potential
    SYSTEM_USER that can be accessed through parallel workers, a second
    field is added for the authentication method, copied directly from
    Port.
    
    ClientConnectionInfo is serialized and restored using a new parallel
    key and a structure tracks the length of the authn_id, making the
    addition of more fields straight-forward.
    
    Author: Jacob Champion
    Reviewed-by: Bertrand Drouvot, Stephen Frost, Robert Haas, Tom Lane,
    Michael Paquier, Julien Rouhaud
    Discussion: https://postgr.es/m/793d990837ae5c06a558d58d62de9378ab525d83.camel@vmware.com
    
    (cherry picked from commit d951052a9e02bfacad8bd6f0f53a4dcd3b7e6d1f)

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -4478,6 +4478,7 @@ BackendInitialize(Port *port)
 
 	/* Save port etc. for ps status */
 	MyProcPort = port;
+	memset(&MyClientConnectionInfo, 0, sizeof(MyClientConnectionInfo));
 
 	/* Tell fd.c about the long-lived FD associated with the port */
 	ReserveExternalFD();
