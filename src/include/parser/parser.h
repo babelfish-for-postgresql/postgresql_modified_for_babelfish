0ace59bd2bb0eb67741be3eb97ae515a4150ebb2
Dipesh Dhameliya <dddhamel@amazon.com>
2022-11-25 11:21:24 +0530

    Revert "Restore src/include/parser/parser.h"
    This reverts commit 245d5662a7a61a51c0055d650edc5a1a86f695a1.

--- src/include/parser/parser.h
+++ src/include/parser/parser.h
@@ -83,19 +83,6 @@ typedef enum
 	DATEFIRST_SUNDAY
 } DATEFIRST;
 
-/* Enum declaration to support FOR JSON clause */
-typedef enum
-{
-	TSQL_FORJSON_AUTO,
-	TSQL_FORJSON_PATH,
-} TSQLFORJSONMode;
-
-typedef enum
-{
-	TSQL_JSON_DIRECTIVE_INCLUDE_NULL_VALUES,
-	TSQL_JSON_DIRECTIVE_WITHOUT_ARRAY_WRAPPER
-} TSQLJSONDirective;
-
 /* GUC variables in scan.l (every one of these is a bad idea :-() */
 extern int	backslash_quote;
 extern bool escape_string_warning;
