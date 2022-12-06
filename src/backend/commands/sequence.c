e04eb022c93bfaa61848fa1dd7757bdd74362502
Aditya Verma <45755382+aadityavermaa@users.noreply.github.com>
2022-12-06 23:44:22 +0530

    Support UDTS for sequences and ISC-sequences view (#52)
    Creation of view information_schema_tsql.sequences to support for T-SQL INFORMATION_SCHEMA.SEQUENCES.
    Provide support for creation of a sequence using user-defined data types which is currently not supported in Babelfish.
    Added support for cross-schema UDTs
    
    Task: BABEL-3504, BABEL-3452
    Signed-off-by: Aditya Verma adiityav@amazon.com

--- src/backend/commands/sequence.c
+++ src/backend/commands/sequence.c
@@ -1393,7 +1393,13 @@ init_params(ParseState *pstate, List *options, bool for_identity,
 	/* AS type */
 	if (as_type != NULL)
 	{
-		Oid			newtypid = typenameTypeId(pstate, defGetTypeName(as_type));
+		/*
+		 * typenameTypeID is called after the hook to modify the schema name
+		 * internally used within the function when sequence is defined using a
+		 * user-defined data type
+		 */
+
+		Oid			newtypid = 0;
 
 		if (pltsql_sequence_datatype_hook)
 			(* pltsql_sequence_datatype_hook) (pstate,
@@ -1402,6 +1408,8 @@ init_params(ParseState *pstate, List *options, bool for_identity,
 											   as_type,
 											   &max_value,
 											   &min_value);
+
+		if(!newtypid) newtypid = typenameTypeId(pstate,defGetTypeName(as_type));
 
 		if (newtypid != INT2OID &&
 			newtypid != INT4OID &&
