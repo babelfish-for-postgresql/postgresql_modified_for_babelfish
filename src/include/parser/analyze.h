cdde863ff68ee199340d6c54a1056163e198a318
Xiaohui F <hexiaohu@amazon.com>
2023-01-27 10:52:56 -0800

    Support and re-design UPDATE/DELETE (#85)
    * Support and re-design UPDATE/DELETE
    
    This commit adds a required engine hook for the re-design of
    UPDATE/DELETE DML statemenet.
    
    Hook name: post_set_target_table_hook
    Input argument: (ParseState *pstate, Node *stmt, CmdType command)
    Location: In parse analyzing process of UPDATE/DELETE, after setting
    target table and before analyzing FROM clause
    Usage: This hook is used to clean up duplicate table references
    
    This commit also modifies the input arguments of other two engine hooks,
    pre_output_clause_transformation_hook and pre_transform_returning_hook,
    in order to extend their functionalities.
    
    Signed-off-by: Xiaohui Fanhe <hexiaohu@amazon.com>
    
    * Modify engine hook implementation
    
    Signed-off-by: Xiaohui Fanhe <hexiaohu@amazon.com>
    
    * Rename the engine hook
    
    Signed-off-by: Xiaohui Fanhe <hexiaohu@amazon.com>
    
    ---------
    
    Signed-off-by: Xiaohui Fanhe <hexiaohu@amazon.com>

--- src/include/parser/analyze.h
+++ src/include/parser/analyze.h
@@ -29,9 +29,7 @@ typedef void (*pre_parse_analyze_hook_type) (ParseState *pstate, RawStmt *parseT
 extern PGDLLIMPORT pre_parse_analyze_hook_type pre_parse_analyze_hook;
 
 /* Hook to handle qualifiers in returning list for output clause */
-typedef void (*pre_transform_returning_hook_type) (CmdType command, 
-										List *returningList, ParseState *pstate);
-
+typedef void (*pre_transform_returning_hook_type) (Query *query, List *returningList, ParseState *pstate);
 extern PGDLLIMPORT pre_transform_returning_hook_type pre_transform_returning_hook;
 
 /* Hook to modify insert statement in output clause */
@@ -40,7 +38,7 @@ typedef void (*pre_transform_insert_hook_type) (InsertStmt *stmt, Oid relid);
 extern PGDLLIMPORT pre_transform_insert_hook_type pre_transform_insert_hook;
 
 /* Hook to perform self-join transformation on UpdateStmt in output clause */
-typedef Node* (*pre_output_clause_transformation_hook_type) (ParseState *pstate, UpdateStmt *stmt, CmdType command);
+typedef Node* (*pre_output_clause_transformation_hook_type) (ParseState *pstate, UpdateStmt *stmt, Query *query);
 extern PGDLLIMPORT pre_output_clause_transformation_hook_type pre_output_clause_transformation_hook;
 
 /* Hook to read a global variable with info on output clause */
@@ -50,6 +48,10 @@ extern PGDLLIMPORT get_output_clause_status_hook_type get_output_clause_status_h
 /* Hook for plugins to get control after an insert row transform */
 typedef void (*post_transform_insert_row_hook_type) (List *icolumns, List *exprList, Oid relid);
 extern PGDLLIMPORT post_transform_insert_row_hook_type post_transform_insert_row_hook;
+
+/* Hook for handle target table before transforming from clause */
+typedef int (*set_target_table_alternative_hook_type) (ParseState *pstate, Node *stmt, CmdType command);
+extern PGDLLIMPORT set_target_table_alternative_hook_type set_target_table_alternative_hook;
 
 extern Query *parse_analyze(RawStmt *parseTree, const char *sourceText,
 							Oid *paramTypes, int numParams, QueryEnvironment *queryEnv);
