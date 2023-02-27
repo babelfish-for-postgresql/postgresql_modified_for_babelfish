7244a57dff6d40492e10e5e960aeed8b466b55f8
Tom Lane <tgl@sss.pgh.pa.us>
2023-02-27 17:21:31 -0500

    Suppress compiler warnings in new pgstats code.
    Some clang versions whine about comparing an enum variable to
    a value outside the range of the enum, on the grounds that the
    result must be constant.  In the cases we fix here, the loops
    will terminate only if the enum variable can in fact hold a
    value one beyond its declared range.  While that's very likely
    to always be true for these enum types, it still seems like a
    poor coding practice to assume it; so use "int" loop variables
    instead to silence the warnings.  (This matches what we've done
    in other places, for example loops over the range of ForkNumber.)
    
    While at it, let's drop the XXX_FIRST macros for these enums and just
    write zeroes for the loop start values.  The apparent flexibility
    seems rather illusory given that iterating up to one-less-than-
    the-number-of-values is only correct for a zero-based range.
    
    Melanie Plageman
    
    Discussion: https://postgr.es/m/20520.1677435600@sss.pgh.pa.us
    (cherry picked from commit 728560db7d868b3ded9a8675742083ab89bcff7c)

--- src/include/pgstat.h
+++ src/include/pgstat.h
@@ -288,7 +288,6 @@ typedef enum IOObject
 	IOOBJECT_TEMP_RELATION,
 } IOObject;
 
-#define IOOBJECT_FIRST IOOBJECT_RELATION
 #define IOOBJECT_NUM_TYPES (IOOBJECT_TEMP_RELATION + 1)
 
 typedef enum IOContext
@@ -299,7 +298,6 @@ typedef enum IOContext
 	IOCONTEXT_VACUUM,
 } IOContext;
 
-#define IOCONTEXT_FIRST IOCONTEXT_BULKREAD
 #define IOCONTEXT_NUM_TYPES (IOCONTEXT_VACUUM + 1)
 
 typedef enum IOOp
@@ -312,7 +310,6 @@ typedef enum IOOp
 	IOOP_WRITE,
 } IOOp;
 
-#define IOOP_FIRST IOOP_EVICT
 #define IOOP_NUM_TYPES (IOOP_WRITE + 1)
 
 typedef struct PgStat_BktypeIO
