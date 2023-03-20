9ef2af13549ad06dc3144fdb6bd0cadec128b698
Kritika <kritikaq@amazon.com>
2023-03-20 16:12:18 +0000

    Create proc/func not visible in the pg_stat_statements view
    Subsequent processutility hooks after bbf_processutilty were not being
    called. Created a hook for that and called it inside relevant
    ProcessUtility function.
    
    Signed-off-by: Kritika <kritikaq@amazon.com>

--- src/include/tcop/utility.h
+++ src/include/tcop/utility.h
@@ -15,11 +15,11 @@
 #define UTILITY_H
 
 #include "tcop/cmdtag.h"
-#include "tcop/tcopprot.h"
-#include "tcop/utility.h"
+//#include "tcop/tcopprot.h"
+//#include "tcop/utility.h"
 #include "tcop/dest.h"
-#include "tcop/cmdtag.h"
-#include "parser/parse_type.h"
+#include "parser/parse_node.h"
+//#include "parser/parse_type.h"
 #include "nodes/plannodes.h"
 #include "nodes/params.h"
 
