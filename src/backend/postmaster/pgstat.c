1107a6f058a1fbf725c391e0b928fcaaf86eae0d
Zhibai <songzhibai1234@gmail.com>
2023-03-29 11:54:57 -0700

    Fix the crash when set client_encoding from pg endpoint using babel database
    Previously when try to set variable from pg endpoint when that backend is
    using babel database, the postgres backend will crash.
    
    The root cause for this crash is pg backend when returnning will call
    security definer in report Guc changes, in such condition, it's not
    within a transaction.
    
    This commit fix this issue by checking the transaction state in security
    definer.
    
    Task: BABEL-3301
    Signed-off-by: Zhibai Song <szh@amazon.com>

--- src/backend/postmaster/pgstat.c
+++ src/backend/postmaster/pgstat.c
@@ -1907,7 +1907,7 @@ pgstat_init_function_usage(FunctionCallInfo fcinfo,
 	PgStat_BackendFunctionEntry *htabent;
 	bool		found;
 
-	if (pre_function_call_hook)
+	if (pre_function_call_hook && IsTransactionState())
 	{
 		HeapTuple proctup = SearchSysCache1(PROCOID, ObjectIdGetDatum(fcinfo->flinfo->fn_oid));
 		if (HeapTupleIsValid(proctup))
