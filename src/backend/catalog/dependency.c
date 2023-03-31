ee2494b9daad5d0be50ab2b7714f549f8c5d14f2
Tim Chang <changti@amazon.com>
2023-03-31 06:17:34 +0000

    Add pg_depend, pg_shdepend to ENR for temp tables
    Task: BABEL-3865
    Signed-off-by: Tim Chang <changti@amazon.com>

--- src/backend/catalog/dependency.c
+++ src/backend/catalog/dependency.c
@@ -1355,7 +1355,8 @@ deleteOneObject(const ObjectAddress *object, Relation *depRel, int flags)
 
 	while (HeapTupleIsValid(tup = systable_getnext(scan)))
 	{
-		CatalogTupleDelete(*depRel, &tup->t_self);
+		if (!ENRdropTuple(*depRel, tup))
+			CatalogTupleDelete(*depRel, &tup->t_self);
 	}
 
 	systable_endscan(scan);
