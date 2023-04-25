86db172b587e7a2a736e9c9b3d027ff3edea195e
Michael Paquier <michael@paquier.xyz>
2023-04-25 09:42:36 +0900

    Fix buffer refcount leak with FDW bulk inserts
    The leak would show up when using batch inserts with foreign tables
    included in a partition tree, as the slots used in the batch were not
    reset once processed.  In order to fix this problem, some
    ExecClearTuple() are added to clean up the slots used once a batch is
    filled and processed, mapping with the number of slots currently in use
    as tracked by the counter ri_NumSlots.
    
    This buffer refcount leak has been introduced in b676ac4 with the
    addition of the executor facility to improve bulk inserts for FDWs, so
    backpatch down to 14.
    
    Alexander has provided the patch (slightly modified by me).  The test
    for postgres_fdw comes from me, based on the test case that the author
    has sent in the report.
    
    Author: Alexander Pyhalov
    Discussion: https://postgr.es/m/b035780a740efd38dc30790c76927255@postgrespro.ru
    Backpatch-through: 14
    (cherry picked from commit 4cc56f8edbe6c00ecb66d6b2c775eaf3268706e3)

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -770,7 +770,6 @@ ExecInsert(ModifyTableState *mtstate,
 								resultRelInfo->ri_PlanSlots,
 								resultRelInfo->ri_NumSlots,
 								estate, canSetTag);
-				resultRelInfo->ri_NumSlots = 0;
 				flushed = true;
 			}
 
@@ -1157,6 +1156,14 @@ ExecBatchInsert(ModifyTableState *mtstate,
 
 	if (canSetTag && numInserted > 0)
 		estate->es_processed += numInserted;
+
+	/* Clean up all the slots, ready for the next batch */
+	for (i = 0; i < numSlots; i++)
+	{
+		ExecClearTuple(slots[i]);
+		ExecClearTuple(planSlots[i]);
+	}
+	resultRelInfo->ri_NumSlots = 0;
 }
 
 /* ----------------------------------------------------------------
@@ -1590,7 +1597,6 @@ ExecPendingInserts(EState *estate)
 						resultRelInfo->ri_PlanSlots,
 						resultRelInfo->ri_NumSlots,
 						estate, mtstate->canSetTag);
-		resultRelInfo->ri_NumSlots = 0;
 	}
 
 	list_free(estate->es_insert_pending_result_relations);
