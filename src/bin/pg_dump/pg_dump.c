20016000a22fb94809f54be8232d2a4af4b41d58
Tom Lane <tgl@sss.pgh.pa.us>
2023-06-04 13:05:54 -0400

    Fix pg_dump's failure to honor dependencies of SQL functions.
    A new-style SQL function can contain a parse-time dependency
    on a unique index, much as views and matviews can (such cases
    arise from GROUP BY and ON CONFLICT clauses, for example).
    To dump and restore such a function successfully, pg_dump must
    postpone the function until after the unique index is created,
    which will happen in the post-data part of the dump.  Therefore
    we have to remove the normal constraint that functions are
    dumped in pre-data.  Add code similar to the existing logic
    that handles this for matviews.  I added test cases for both
    as well, since code coverage tests showed that we weren't
    testing the matview logic.
    
    Per report from Sami Imseih.  Back-patch to v14 where
    new-style SQL functions came in.
    
    Discussion: https://postgr.es/m/2C1933AB-C2F8-499B-9D18-4AC1882256A0@amazon.com
    (cherry picked from commit d6f549d7a6275e16668faf8cddc95d6759506108)

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -6078,6 +6078,7 @@ getAggregates(Archive *fout, int *numAggs)
 						  agginfo[i].aggfn.argtypes,
 						  agginfo[i].aggfn.nargs);
 		}
+		agginfo[i].aggfn.postponed_def = false; /* might get set during sort */
 
 		/* Decide whether we want to dump it */
 		selectDumpableObject(&(agginfo[i].aggfn.dobj), fout);
@@ -6308,6 +6309,7 @@ getFuncs(Archive *fout, int *numFuncs)
 			parseOidArray(PQgetvalue(res, i, i_proargtypes),
 						  finfo[i].argtypes, finfo[i].nargs);
 		}
+		finfo[i].postponed_def = false; /* might get set during sort */
 
 		/* Decide whether we want to dump it */
 		selectDumpableObject(&(finfo[i].dobj), fout);
@@ -12729,7 +12731,8 @@ dumpFunc(Archive *fout, const FuncInfo *finfo)
 								  .namespace = finfo->dobj.namespace->dobj.name,
 								  .owner = finfo->rolname,
 								  .description = keyword,
-								  .section = SECTION_PRE_DATA,
+								  .section = finfo->postponed_def ?
+								  SECTION_POST_DATA : SECTION_PRE_DATA,
 								  .createStmt = q->data,
 								  .dropStmt = delqry->data));
 
