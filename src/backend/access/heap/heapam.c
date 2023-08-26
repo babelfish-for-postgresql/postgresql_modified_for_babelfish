c61adce5358e99536d3bd0d2ce28abcf5b55008e
Kristian Lejao <1741885+lejaokri@users.noreply.github.com>
2023-08-25 17:35:40 -0700

    Fixed wrong call to HeapTupleSatisfiesVisibility (#196)
    Task: MANFRED-18764
    
    Signed-off-by: Kristian Lejao <klejao@amazon.com>
    (cherry picked from commit bd8b2954f12b6b8a941f9b7b9499d6f8c38790f5)

--- src/backend/access/heap/heapam.c
+++ src/backend/access/heap/heapam.c
@@ -2866,7 +2866,7 @@ l1:
 	if (crosscheck != InvalidSnapshot && result == TM_Ok)
 	{
 		/* Perform additional check for transaction-snapshot mode RI updates */
-		if (HeapTupleSatisfiesVisibility(relation, &tp, crosscheck, buffer))
+		if (!HeapTupleSatisfiesVisibility(relation, &tp, crosscheck, buffer))
 			result = TM_Updated;
 	}
 
@@ -3493,7 +3493,7 @@ l2:
 	if (crosscheck != InvalidSnapshot && result == TM_Ok)
 	{
 		/* Perform additional check for transaction-snapshot mode RI updates */
-		if (HeapTupleSatisfiesVisibility(relation, &oldtup, crosscheck, buffer))
+		if (!HeapTupleSatisfiesVisibility(relation, &oldtup, crosscheck, buffer))
 		{
 			result = TM_Updated;
 			Assert(!ItemPointerEquals(&oldtup.t_self, &oldtup.t_data->t_ctid));
