9a730056b02cea7e44aad8e61bfabc9825218555
Dipesh Dhameliya <dddhamel@amazon.com>
2023-08-29 11:40:37 +0530

    Check the dialect before firing INSTEAD OF EACH STATEMENT triggers

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -2575,7 +2575,7 @@ ExecModifyTable(PlanState *pstate)
 	 * 
 	 * IOT should be fired after execution is done
 	 */
-	if (node->fireISTriggers == IOT_NOT_FIRED)
+	if (node->fireISTriggers == IOT_NOT_FIRED && sql_dialect == SQL_DIALECT_TSQL)
 	{
 		node->fireISTriggers = fireISTriggers(node);
 	}
