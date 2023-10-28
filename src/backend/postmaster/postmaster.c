2c1014b5320c814136cf99db3d82d793d1283477
Bruce Momjian <bruce@momjian.us>
2023-10-28 12:58:32 -0400

    doc:  comment wording improvement
    Discussion: https://postgr.es/m/CAEG8a3L7UoZXH1VmzpV-VDkex2kt68nWKuW1WiohoT=RrzYKWA@mail.gmail.com
    
    Author: Junwang Zhao
    
    Backpatch-through: master
    (cherry picked from commit de64268561606b62e69e81b9c07ac2eab93c85ea)

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -1694,8 +1694,8 @@ getInstallationPaths(const char *argv0)
 	FreeDir(pdir);
 
 	/*
-	 * XXX is it worth similarly checking the share/ directory?  If the lib/
-	 * directory is there, then share/ probably is too.
+	 * It's not worth checking the share/ directory.  If the lib/ directory
+	 * is there, then share/ probably is too.
 	 */
 }
 
