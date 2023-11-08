df059d27a46fd5dc429dfb9eaaca9fb1ef98d4ba
Deepakshi Mittal <78574784+deepakshi-mittal@users.noreply.github.com>
2023-11-08 12:33:47 -0800

    Fix for Order by issue in select into statement with identity function (#247)
    Modified transformSelectIntoStmt, remove unused variable queryString
    Modified SortClause and Distinct Clause of select into, removed identity function if exists in it
    Added alter table add identity statement which will be executed after select into statement
    Added IsA check for SortGroupClause
    Removed unused variable tsql_select_into_seq_oid
    Removed smallint and int functions,
    Dropped function objects from upgrade scripts
    Used strcasecmp inplace of strncasecmp
    
    Added test cases for select into identity with
    
    top clause
    distinct clause
    order by clause
    
    Engine Pr:babelfish-for-postgresql/postgresql_modified_for_babelfish#247
    
    Task: BABEL-4388, BABEL-539
    Signed-off-by: Deepakshi Mittal <depakshi@amazon.com>

--- src/backend/nodes/outfuncs.c
+++ src/backend/nodes/outfuncs.c
@@ -1134,8 +1134,6 @@ _outIntoClause(StringInfo str, const IntoClause *node)
 	WRITE_STRING_FIELD(tableSpaceName);
 	WRITE_NODE_FIELD(viewQuery);
 	WRITE_BOOL_FIELD(skipData);
-	WRITE_STRING_FIELD(identityName);
-	WRITE_STRING_FIELD(identityType);
 }
 
 static void
