83a0623cf4dcbc9ce4dffed82f57a5e3f65badb5
Zhibai Song <szh@amazon.com>
2023-11-21 19:28:46 +0000

    Update due to comment

--- src/backend/optimizer/util/clauses.c
+++ src/backend/optimizer/util/clauses.c
@@ -4231,11 +4231,17 @@ fetch_function_defaults(HeapTuple func_tuple)
 	proargdefaults = SysCacheGetAttr(PROCOID, func_tuple,
 									 Anum_pg_proc_proargdefaults,
 									 &isnull);
-	if (isnull && sql_dialect != SQL_DIALECT_TSQL)
-	// We don't want to see this error in TSQL
+	if (isnull)
+	{
+		/* This error msg is not expected in Tsql for 2 reasons
+		   1. Procedure call will throw a specific error msg later
+		   2. Func call even without defining default value, it 
+		    won't error out, it'll use NULL instead */
+		if (sql_dialect == SQL_DIALECT_TSQL)
+			return NIL;
+
 		elog(ERROR, "not enough default arguments");
-	if (isnull && sql_dialect == SQL_DIALECT_TSQL)
-		return NIL;
+	}
 	str = TextDatumGetCString(proargdefaults);
 	defaults = castNode(List, stringToNode(str));
 	pfree(str);
