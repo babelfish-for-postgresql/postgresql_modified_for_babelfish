4531c7b6667dafbfaf7779a535a26f2ee1346835
Yanjie Xu <yanjiex@amazon.com>
2023-11-22 16:18:44 -0800

    [BABEL-284] Support table name prefix in pivot for column (#255)
    Support table name prefix in pivot for column
    
    Task: BABEL-284
    Signed-off-by: Yanjie Xu <yanjiex@amazon.com>

--- src/include/nodes/parsenodes.h
+++ src/include/nodes/parsenodes.h
@@ -1994,9 +1994,9 @@ typedef struct SelectStmt
 	bool	isPivot;
 	struct SelectStmt *srcSql;
 	struct SelectStmt *catSql;
-	List	*value_col_strlist;
-	char	*pivotCol;
-	Node	*aggFunc;
+	List		*value_col_strlist;
+	ColumnRef	*pivotCol;
+	Node		*aggFunc;
 } SelectStmt;
 
 
