sanchitachaurasia/DBIS-Project

Rearrange some static assertions for consistency
Put lengthof first.

Reported-by: Peter Smith <smithpb2250@gmail.com>
Discussion: https://www.postgresql.org/message-id/CAHut+PsUDMySVRuRc=h+P5N3+=TGvj4W_mi32XXg9dt4o-BXbA@mail.gmail.com

src/backend/utils/cache/syscache.c
@@ -1068,7 +1068,7 @@ InitCatalogCache(void)
 {
 	int			cacheId;
 
-	StaticAssertStmt(SysCacheSize == (int) lengthof(cacheinfo),
+	StaticAssertStmt(lengthof(cacheinfo) == SysCacheSize,
 					 "SysCacheSize does not match syscache.c's array");
 
 	Assert(!CacheInitialized);

babelfish-for-postgresql/postgresql_modified_for_babelfish

BABELFISH-CONFLICT: see Postgres community repo for original commit
(cherry picked from commit b18c2decd76eeffbd483c041c02bb0fb01b0f124)

src/backend/utils/cache/syscache.c
@@ -1056,7 +1056,7 @@ InitCatalogCache(void)
 {
 	int			cacheId;
 
-	StaticAssertStmt(SysCacheNoExtensionSize == (int) lengthof(cacheinfo),
+	StaticAssertStmt(lengthof(cacheinfo) == SysCacheNoExtensionSize,
 					 "SysCacheSize does not match syscache.c's array");
 
 	Assert(!CacheInitialized);