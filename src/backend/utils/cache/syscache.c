6ff921aeea5518e70801ec7e41fd7e597dd094d3
Shameem Ahmed <shameemahmed20apr2000@gmail.com>
2023-12-08 10:13:05 +0000

    [BABELFISH] Remove redundant cachedesc struct declaration
    Signed-off-by: Shameem Ahmed <shameemahmed20apr2000@gmail.com>

--- src/backend/utils/cache/syscache.c
+++ src/backend/utils/cache/syscache.c
@@ -110,18 +110,6 @@
 *---------------------------------------------------------------------------
 */
 
-/*
- *		struct cachedesc: information defining a single syscache
- */
-struct cachedesc
-{
-	Oid			reloid;			/* OID of the relation being cached */
-	Oid			indoid;			/* OID of index relation for this cache */
-	int			nkeys;			/* # of keys needed for cache lookup */
-	int			key[4];			/* attribute numbers of key attrs */
-	int			nbuckets;		/* number of hash buckets for this cache */
-};
-
 /* Macro to provide nkeys and key array with convenient syntax. */
 #define KEY(...) VA_ARGS_NARGS(__VA_ARGS__), { __VA_ARGS__ }
 
