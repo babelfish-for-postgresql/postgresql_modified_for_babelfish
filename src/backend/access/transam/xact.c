34699ceef6bbfa71a546ed7c8064d06efb56189c
Michael Paquier <michael@paquier.xyz>
2023-12-12 17:05:27 +0100

    Prevent tuples to be marked as dead in subtransactions on standbys
    Dead tuples are ignored and are not marked as dead during recovery, as
    it can lead to MVCC issues on a standby because its xmin may not match
    with the primary.  This information is tracked by a field called
    "xactStartedInRecovery" in the transaction state data, switched on when
    starting a transaction in recovery.
    
    Unfortunately, this information was not correctly tracked when starting
    a subtransaction, because the transaction state used for the
    subtransaction did not update "xactStartedInRecovery" based on the state
    of its parent.  This would cause index scans done in subtransactions to
    return inconsistent data, depending on how the xmin of the primary
    and/or the standby evolved.
    
    This is broken since the introduction of hot standby in efc16ea52067, so
    backpatch all the way down.
    
    Author: Fei Changhong
    Reviewed-by: Kyotaro Horiguchi
    Discussion: https://postgr.es/m/tencent_C4D907A5093C071A029712E73B43C6512706@qq.com
    Backpatch-through: 12
    (cherry picked from commit 0e2c05af90ee51f87564e37abdd1df1a8f554064)

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -5292,6 +5292,7 @@ PushTransaction(void)
 	s->blockState = TBLOCK_SUBBEGIN;
 	GetUserIdAndSecContext(&s->prevUser, &s->prevSecContext);
 	s->prevXactReadOnly = XactReadOnly;
+	s->startedInRecovery = p->startedInRecovery;
 	s->parallelModeLevel = 0;
 	s->topXidLogged = false;
 
