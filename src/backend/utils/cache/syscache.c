e2ae2bac28483ff59563aa8db672f903eb4c4c9e
Nathan Bossart <nathan@postgresql.org>
2024-02-16 14:05:36 -0600

    Use new overflow-safe integer comparison functions.
    Commit 6b80394781 introduced integer comparison functions designed
    to be as efficient as possible while avoiding overflow.  This
    commit makes use of these functions in many of the in-tree qsort()
    comparators to help ensure transitivity.  Many of these comparator
    functions should also see a small performance boost.
    
    Author: Mats Kindahl
    Reviewed-by: Andres Freund, Fabrízio de Royes Mello
    Discussion: https://postgr.es/m/CA%2B14426g2Wa9QuUpmakwPxXFWG_1FaY0AsApkvcTBy-YfS6uaw%40mail.gmail.com
    (cherry picked from commit 3b42bdb47169c617cb79123c407a19d16458b49a)

--- src/backend/utils/cache/syscache.c
+++ src/backend/utils/cache/syscache.c
@@ -31,6 +31,7 @@
 #include "catalog/pg_shdepend_d.h"
 #include "catalog/pg_shdescription_d.h"
 #include "catalog/pg_shseclabel_d.h"
+#include "common/int.h"
 #include "lib/qunique.h"
 #include "utils/catcache.h"
 #include "utils/lsyscache.h"
@@ -702,7 +703,5 @@ oid_compare(const void *a, const void *b)
 	Oid			oa = *((const Oid *) a);
 	Oid			ob = *((const Oid *) b);
 
-	if (oa == ob)
-		return 0;
-	return (oa > ob) ? 1 : -1;
+	return pg_cmp_u32(oa, ob);
 }
