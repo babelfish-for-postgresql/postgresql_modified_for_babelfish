bb4002e2e7c1d82e63552599b5fb44509ef537ba
Jeff Davis <jdavis@postgresql.org>
2024-03-04 17:31:38 -0800

    Fix search_path to a safe value during maintenance operations.
    While executing maintenance operations (ANALYZE, CLUSTER, REFRESH
    MATERIALIZED VIEW, REINDEX, or VACUUM), set search_path to
    'pg_catalog, pg_temp' to prevent inconsistent behavior.
    
    Functions that are used for functional indexes, in index expressions,
    or in materialized views and depend on a different search path must be
    declared with CREATE FUNCTION ... SET search_path='...'.
    
    This change was previously committed as 05e1737351, then reverted in
    commit 2fcc7ee7af because it was too late in the cycle.
    
    Preparation for the MAINTAIN privilege, which was previously reverted
    due to search_path manipulation hazards.
    
    Discussion: https://postgr.es/m/d4ccaf3658cb3c281ec88c851a09733cd9482f22.camel@j-davis.com
    Discussion: https://postgr.es/m/E1q7j7Y-000z1H-Hr%40gemulon.postgresql.org
    Discussion: https://postgr.es/m/e44327179e5c9015c8dda67351c04da552066017.camel%40j-davis.com
    Reviewed-by: Greg Stark, Nathan Bossart, Noah Misch
    (cherry picked from commit 2af07e2f749a9208ca1ed84fa1d8fe0e75833288)

--- src/backend/catalog/index.c
+++ src/backend/catalog/index.c
@@ -1491,6 +1491,8 @@ index_concurrently_build(Oid heapRelationId,
 	SetUserIdAndSecContext(heapRel->rd_rel->relowner,
 						   save_sec_context | SECURITY_RESTRICTED_OPERATION);
 	save_nestlevel = NewGUCNestLevel();
+	SetConfigOption("search_path", GUC_SAFE_SEARCH_PATH, PGC_USERSET,
+					PGC_S_SESSION);
 
 	indexRelation = index_open(indexRelationId, RowExclusiveLock);
 
@@ -3047,6 +3049,9 @@ index_build(Relation heapRelation,
 	SetUserIdAndSecContext(heapRelation->rd_rel->relowner,
 						   save_sec_context | SECURITY_RESTRICTED_OPERATION);
 	save_nestlevel = NewGUCNestLevel();
+	if (!IsBootstrapProcessingMode())
+		SetConfigOption("search_path", GUC_SAFE_SEARCH_PATH, PGC_USERSET,
+						PGC_S_SESSION);
 
 	/* Set up initial progress report status */
 	{
@@ -3382,6 +3387,8 @@ validate_index(Oid heapId, Oid indexId, Snapshot snapshot)
 	SetUserIdAndSecContext(heapRelation->rd_rel->relowner,
 						   save_sec_context | SECURITY_RESTRICTED_OPERATION);
 	save_nestlevel = NewGUCNestLevel();
+	SetConfigOption("search_path", GUC_SAFE_SEARCH_PATH, PGC_USERSET,
+					PGC_S_SESSION);
 
 	indexRelation = index_open(indexId, RowExclusiveLock);
 
@@ -3643,6 +3650,8 @@ reindex_index(const ReindexStmt *stmt, Oid indexId,
 	SetUserIdAndSecContext(heapRelation->rd_rel->relowner,
 						   save_sec_context | SECURITY_RESTRICTED_OPERATION);
 	save_nestlevel = NewGUCNestLevel();
+	SetConfigOption("search_path", GUC_SAFE_SEARCH_PATH, PGC_USERSET,
+					PGC_S_SESSION);
 
 	if (progress)
 	{
