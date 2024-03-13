2bd43fe0ef5b3f84a3191a50ce43c3d25c701bca
Peter Eisentraut <peter@eisentraut.org>
2024-03-13 15:07:00 +0100

    Make the order of the header file includes consistent
    BABELFISH-CONFLICT: see Postgres community repo for original commit
    
    Similar to commit 7e735035f20.
    
    Author: Richard Guo <guofenglinux@gmail.com>
    Reviewed-by: Bharath Rupireddy <bharath.rupireddyforpostgres@gmail.com>
    Discussion: https://www.postgresql.org/message-id/flat/CAMbWs4-WhpCFMbXCjtJ%2BFzmjfPrp7Hw1pk4p%2BZpU95Kh3ofZ1A%40mail.gmail.com
    (cherry picked from commit 97d85be365443eb4bf84373a7468624762382059)

--- src/backend/utils/activity/pgstat_relation.c
+++ src/backend/utils/activity/pgstat_relation.c
@@ -19,12 +19,12 @@
 
 #include "access/twophase_rmgr.h"
 #include "access/xact.h"
+#include "catalog/catalog.h"
 #include "postmaster/autovacuum.h"
 #include "utils/memutils.h"
 #include "utils/pgstat_internal.h"
 #include "utils/rel.h"
 #include "utils/timestamp.h"
-#include "catalog/catalog.h"
 #include "utils/guc.h"
 
 #include "parser/parser.h"
