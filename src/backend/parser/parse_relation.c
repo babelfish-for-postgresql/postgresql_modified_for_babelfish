5d15d477e8d7eba87a00ed774dd398a256cd7900
Amit Langote <amitlan@postgresql.org>
2024-04-04 19:57:08 +0900

    Add basic JSON_TABLE() functionality
    JSON_TABLE() allows JSON data to be converted into a relational view
    and thus used, for example, in a FROM clause, like other tabular
    data.  Data to show in the view is selected from a source JSON object
    using a JSON path expression to get a sequence of JSON objects that's
    called a "row pattern", which becomes the source to compute the
    SQL/JSON values that populate the view's output columns.  Column
    values themselves are computed using JSON path expressions applied to
    each of the JSON objects comprising the "row pattern", for which the
    SQL/JSON query functions added in 6185c9737cf4 are used.
    
    To implement JSON_TABLE() as a table function, this augments the
    TableFunc and TableFuncScanState nodes that are currently used to
    support XMLTABLE() with some JSON_TABLE()-specific fields.
    
    Note that the JSON_TABLE() spec includes NESTED COLUMNS and PLAN
    clauses, which are required to provide more flexibility to extract
    data out of nested JSON objects, but they are not implemented here
    to keep this commit of manageable size.
    
    Author: Nikita Glukhov <n.gluhov@postgrespro.ru>
    Author: Teodor Sigaev <teodor@sigaev.ru>
    Author: Oleg Bartunov <obartunov@gmail.com>
    Author: Alexander Korotkov <aekorotkov@gmail.com>
    Author: Andrew Dunstan <andrew@dunslane.net>
    Author: Amit Langote <amitlangote09@gmail.com>
    Author: Jian He <jian.universality@gmail.com>
    
    Reviewers have included (in no particular order):
    
    Andres Freund, Alexander Korotkov, Pavel Stehule, Andrew Alsup,
    Erik Rijkers, Zihong Yu, Himanshu Upadhyaya, Daniel Gustafsson,
    Justin Pryzby, Álvaro Herrera, Jian He
    
    Discussion: https://postgr.es/m/cd0bb935-0158-78a7-08b5-904886deac4b@postgrespro.ru
    Discussion: https://postgr.es/m/20220616233130.rparivafipt6doj3@alap3.anarazel.de
    Discussion: https://postgr.es/m/abd9b83b-aa66-f230-3d6d-734817f0995d%40postgresql.org
    Discussion: https://postgr.es/m/CA+HiwqE4XTdfb1nW=Ojoy_tQSRhYt-q_kb6i5d4xcKyrLC1Nbg@mail.gmail.com
    (cherry picked from commit de3600452b61d1bc3967e9e37e86db8956c8f577)

--- src/backend/parser/parse_relation.c
+++ src/backend/parser/parse_relation.c
@@ -2099,8 +2099,6 @@ addRangeTableEntryForTableFunc(ParseState *pstate,
 	Assert(list_length(tf->coltypmods) == list_length(tf->colnames));
 	Assert(list_length(tf->colcollations) == list_length(tf->colnames));
 
-	refname = alias ? alias->aliasname : pstrdup("xmltable");
-
 	rte->rtekind = RTE_TABLEFUNC;
 	rte->relid = InvalidOid;
 	rte->subquery = NULL;
@@ -2110,6 +2108,8 @@ addRangeTableEntryForTableFunc(ParseState *pstate,
 	rte->colcollations = tf->colcollations;
 	rte->alias = alias;
 
+	refname = alias ? alias->aliasname :
+		pstrdup(tf->functype == TFT_XMLTABLE ? "xmltable" : "json_table");
 	eref = alias ? copyObject(alias) : makeAlias(refname, NIL);
 	numaliases = list_length(eref->colnames);
 
@@ -2122,7 +2122,7 @@ addRangeTableEntryForTableFunc(ParseState *pstate,
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_COLUMN_REFERENCE),
 				 errmsg("%s function has %d columns available but %d columns specified",
-						"XMLTABLE",
+						tf->functype == TFT_XMLTABLE ? "XMLTABLE" : "JSON_TABLE",
 						list_length(tf->colnames), numaliases)));
 
 	rte->eref = eref;
