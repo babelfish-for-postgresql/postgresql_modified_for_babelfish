fc3ba9cc71893d0d15fb388a214f72cc1be9f118
Tomas Vondra <tomas.vondra@postgresql.org>
2024-04-07 00:24:12 +0200

    BitmapHeapScan: Push skip_fetch optimization into table AM
    Commit 7c70996ebf0949b142 introduced an optimization to allow bitmap
    scans to operate like index-only scans by not fetching a block from the
    heap if none of the underlying data is needed and the block is marked
    all visible in the visibility map.
    
    With the introduction of table AMs, a FIXME was added to this code
    indicating that the skip_fetch logic should be pushed into the table
    AM-specific code, as not all table AMs may use a visibility map in the
    same way.
    
    This commit resolves this FIXME for the current block. The layering
    violation is still present in BitmapHeapScans's prefetching code, which
    uses the visibility map to decide whether or not to prefetch a block.
    However, this can be addressed independently.
    
    Author: Melanie Plageman
    Reviewed-by: Andres Freund, Heikki Linnakangas, Tomas Vondra, Mark Dilger
    Discussion: https://postgr.es/m/CAAKRu_ZwCwWFeL_H3ia26bP2e7HiKLWt0ZmGXPVwPO6uXq0vaA%40mail.gmail.com
    (cherry picked from commit 04e72ed617be354a53a076b76c6644e364ed80a3)

--- src/backend/access/heap/heapam.c
+++ src/backend/access/heap/heapam.c
@@ -969,6 +969,8 @@ heap_beginscan(Relation relation, Snapshot snapshot,
 	scan->rs_base.rs_flags = flags;
 	scan->rs_base.rs_parallel = parallel_scan;
 	scan->rs_strategy = NULL;	/* set in initscan */
+	scan->rs_vmbuffer = InvalidBuffer;
+	scan->rs_empty_tuples_pending = 0;
 
 	/*
 	 * Disable page-at-a-time mode if it's not a MVCC-safe snapshot.
@@ -1057,6 +1059,14 @@ heap_rescan(TableScanDesc sscan, ScanKey key, bool set_params,
 	if (BufferIsValid(scan->rs_cbuf))
 		ReleaseBuffer(scan->rs_cbuf);
 
+	if (BufferIsValid(scan->rs_vmbuffer))
+	{
+		ReleaseBuffer(scan->rs_vmbuffer);
+		scan->rs_vmbuffer = InvalidBuffer;
+	}
+
+	Assert(scan->rs_empty_tuples_pending == 0);
+
 	/*
 	 * reinitialize scan descriptor
 	 */
@@ -1075,6 +1085,11 @@ heap_endscan(TableScanDesc sscan)
 	 */
 	if (BufferIsValid(scan->rs_cbuf))
 		ReleaseBuffer(scan->rs_cbuf);
+
+	if (BufferIsValid(scan->rs_vmbuffer))
+		ReleaseBuffer(scan->rs_vmbuffer);
+
+	Assert(scan->rs_empty_tuples_pending == 0);
 
 	/*
 	 * decrement relation reference count and free scan descriptor storage
