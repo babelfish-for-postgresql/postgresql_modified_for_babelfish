c10f2e6e80c322fce9f774cad54d449e14ee601b
Masahiko Sawada <msawada@postgresql.org>
2024-04-11 17:18:05 +0900

    Revert indexed and enlargable binary heap implementation.
    This reverts commit b840508644 and bcb14f4abc. These commits were made
    for commit 5bec1d6bc5 (Improve eviction algorithm in ReorderBuffer
    using max-heap for many subtransactions). However, per discussion,
    commit efb8acc0d0 replaced binary heap + index with pairing heap, and
    made these commits unnecessary.
    
    Reported-by: Jeff Davis
    Discussion: https://postgr.es/m/12747c15811d94efcc5cda72d6b35c80d7bf3443.camel%40j-davis.com
    (cherry picked from commit 810f64a01567610af7b92b0de930f16f3e20064e)

--- src/bin/pg_dump/pg_backup_archiver.c
+++ src/bin/pg_dump/pg_backup_archiver.c
@@ -4211,7 +4211,6 @@ restore_toc_entries_parallel(ArchiveHandle *AH, ParallelState *pstate,
 	/* Set up ready_heap with enough room for all known TocEntrys */
 	ready_heap = binaryheap_allocate(AH->tocCount,
 									 TocEntrySizeCompareBinaryheap,
-									 false,
 									 NULL);
 
 	/*
