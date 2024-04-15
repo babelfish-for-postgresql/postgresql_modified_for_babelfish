dde0e1eb71060298bdab065bb5b58bb05ab0ba9d
Sai Rohan Basa <bsrohan@amazon.com>
2024-04-15 06:18:30 +0000

    Test

--- src/backend/parser/parse_expr.c
+++ src/backend/parser/parse_expr.c
@@ -2316,8 +2316,8 @@ transformCoalesceExpr(ParseState *pstate, CoalesceExpr *c)
 
 	if (sql_dialect == SQL_DIALECT_TSQL && select_common_type_hook && c->tsql_is_null)
 		newc->coalescetype = select_common_type(pstate, newargs, "ISNULL", NULL);
-	else if(sql_dialect == SQL_DIALECT_TSQL && select_common_type_hook && c->tsql_coalesce_func)
-		newc->coalescetype = select_common_type(pstate, newargs, "TSQL_COALESCE", NULL);
+	// else if(sql_dialect == SQL_DIALECT_TSQL && select_common_type_hook && c->tsql_coalesce_func)
+	// 	newc->coalescetype = select_common_type(pstate, newargs, "TSQL_COALESCE", NULL);
 	else
 		newc->coalescetype = select_common_type(pstate, newargs, "COALESCE", NULL);
 	/* coalescecollid will be set by parse_collate.c */
