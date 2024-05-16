30d5cd63c56c8e1aa334dc6e395441e5de933aab
Peter Eisentraut <peter@eisentraut.org>
2024-05-16 08:15:35 +0200

    Revert temporal primary keys and foreign keys
    This feature set did not handle empty ranges correctly, and it's now
    too late for PostgreSQL 17 to fix it.
    
    The following commits are reverted:
    
        6db4598fcb8 Add stratnum GiST support function
        46a0cd4cefb Add temporal PRIMARY KEY and UNIQUE constraints
        86232a49a43 Fix comment on gist_stratnum_btree
        030e10ff1a3 Rename pg_constraint.conwithoutoverlaps to conperiod
        a88c800deb6 Use daterange and YMD in without_overlaps tests instead of tsrange.
        5577a71fb0c Use half-open interval notation in without_overlaps tests
        34768ee3616 Add temporal FOREIGN KEY contraints
        482e108cd38 Add test for REPLICA IDENTITY with a temporal key
        c3db1f30cba doc:  clarify PERIOD and WITHOUT OVERLAPS in CREATE TABLE
        144c2ce0cc7 Fix ON CONFLICT DO NOTHING/UPDATE for temporal indexes
    
    Discussion: https://www.postgresql.org/message-id/d0b64a7a-dfe4-4b84-a906-c7dedfa40a3e@eisentraut.org
    (cherry picked from commit 8aee330af55d8a759b2b73f5a771d9d34a7b887f)

--- src/backend/catalog/heap.c
+++ src/backend/catalog/heap.c
@@ -2293,7 +2293,6 @@ StoreRelCheck(Relation rel, const char *ccname, Node *expr,
 							  is_local, /* conislocal */
 							  inhcount, /* coninhcount */
 							  is_no_inherit,	/* connoinherit */
-							  false,	/* conperiod */
 							  is_internal); /* internally constructed? */
 
 	pfree(ccbin);
--- src/backend/utils/adt/ruleutils.c
+++ src/backend/utils/adt/ruleutils.c
@@ -340,7 +340,7 @@ static char *pg_get_viewdef_worker(Oid viewoid,
 								   int prettyFlags, int wrapColumn);
 static char *pg_get_triggerdef_worker(Oid trigid, bool pretty);
 static int	decompile_column_index_array(Datum column_index_array, Oid relId,
-										 bool withPeriod, StringInfo buf);
+										 StringInfo buf);
 static char *pg_get_ruledef_worker(Oid ruleoid, int prettyFlags);
 static char *pg_get_indexdef_worker(Oid indexrelid, int colno,
 									const Oid *excludeOps,
@@ -2262,8 +2262,7 @@ pg_get_constraintdef_worker(Oid constraintId, bool fullCommand,
 				val = SysCacheGetAttrNotNull(CONSTROID, tup,
 											 Anum_pg_constraint_conkey);
 
-				/* If it is a temporal foreign key then it uses PERIOD. */
-				decompile_column_index_array(val, conForm->conrelid, conForm->conperiod, &buf);
+				decompile_column_index_array(val, conForm->conrelid, &buf);
 
 				/* add foreign relation name */
 				appendStringInfo(&buf, ") REFERENCES %s(",
@@ -2274,7 +2273,7 @@ pg_get_constraintdef_worker(Oid constraintId, bool fullCommand,
 				val = SysCacheGetAttrNotNull(CONSTROID, tup,
 											 Anum_pg_constraint_confkey);
 
-				decompile_column_index_array(val, conForm->confrelid, conForm->conperiod, &buf);
+				decompile_column_index_array(val, conForm->confrelid, &buf);
 
 				appendStringInfoChar(&buf, ')');
 
@@ -2360,7 +2359,7 @@ pg_get_constraintdef_worker(Oid constraintId, bool fullCommand,
 				if (!isnull)
 				{
 					appendStringInfoString(&buf, " (");
-					decompile_column_index_array(val, conForm->conrelid, false, &buf);
+					decompile_column_index_array(val, conForm->conrelid, &buf);
 					appendStringInfoChar(&buf, ')');
 				}
 
@@ -2395,9 +2394,7 @@ pg_get_constraintdef_worker(Oid constraintId, bool fullCommand,
 				val = SysCacheGetAttrNotNull(CONSTROID, tup,
 											 Anum_pg_constraint_conkey);
 
-				keyatts = decompile_column_index_array(val, conForm->conrelid, false, &buf);
-				if (conForm->conperiod)
-					appendStringInfoString(&buf, " WITHOUT OVERLAPS");
+				keyatts = decompile_column_index_array(val, conForm->conrelid, &buf);
 
 				appendStringInfoChar(&buf, ')');
 
@@ -2579,7 +2576,7 @@ pg_get_constraintdef_worker(Oid constraintId, bool fullCommand,
  */
 static int
 decompile_column_index_array(Datum column_index_array, Oid relId,
-							 bool withPeriod, StringInfo buf)
+							 StringInfo buf)
 {
 	Datum	   *keys;
 	int			nKeys;
@@ -2598,9 +2595,7 @@ decompile_column_index_array(Datum column_index_array, Oid relId,
 		if (j == 0)
 			appendStringInfoString(buf, quote_identifier(colName));
 		else
-			appendStringInfo(buf, ", %s%s",
-							 (withPeriod && j == nKeys - 1) ? "PERIOD " : "",
-							 quote_identifier(colName));
+			appendStringInfo(buf, ", %s", quote_identifier(colName));
 	}
 
 	return nKeys;
