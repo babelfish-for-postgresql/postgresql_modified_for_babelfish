a4f0aa5f8ce8a940be1522891b4430369b37cb6e
Peter Eisentraut <peter@eisentraut.org>
2024-07-02 09:24:04 +0200

    Limit max parameter number with MaxAllocSize
    MaxAllocSize puts an upper bound on the largest possible parameter
    number ($268435455).  Use that limit instead of INT_MAX to report that
    no parameters exist beyond that point instead of reporting an error
    about the maximum allocation size being exceeded.
    
    Author: Erik Wienhold <ewie@ewie.name>
    Discussion: https://www.postgresql.org/message-id/flat/5d216d1c-91f6-4cbe-95e2-b4cbd930520c@ewie.name
    (cherry picked from commit 9c2e660b07fc16bdd79c25047cce6cde2acb3f37)

--- src/test/regress/sql/prepare.sql
+++ src/test/regress/sql/prepare.sql
@@ -78,22 +78,10 @@ PREPARE q8 AS
 SELECT name, statement, parameter_types, result_types FROM pg_prepared_statements
     ORDER BY name;
 
-CREATE TABLE union_type (id int primary key, name varchar(20));
-
-PREPARE q9
-AS
-WITH temp1 AS (
-    SELECT * from union_type
-    UNION
-    SELECT
-        $1 AS id,
-        $2 AS name
-)
-SELECT * from temp1;
+-- max parameter number and one above
+PREPARE q9 AS SELECT $268435455, $268435456;
 
 -- test DEALLOCATE ALL;
 DEALLOCATE ALL;
 SELECT name, statement, parameter_types FROM pg_prepared_statements
     ORDER BY name;
-
-DROP table union_type;
