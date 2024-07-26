46e0b9e3ef217ba755cadc80a77638af5d6136aa
Tanzeel Khan <140405735+tanscorpio7@users.noreply.github.com>
2024-07-26 22:01:21 +0530

    reset proconfig for functions when babelfish is installed (#382) (#420)
    If babelfish is installed the set_sql_dialect hook is installed. That means we are never resetting the proconfig values for functions with proconfig.
    One major symptom of this is mVU failure when there are other extensions installed which executes a trigger which changes search path as part of proconfig.
    
    Task: BABEL-4982
    
    Signed-off-by: Tanzeel Khan <tzlkhan@amazon.com>

--- src/backend/utils/fmgr/fmgr.c
+++ src/backend/utils/fmgr/fmgr.c
@@ -883,6 +883,8 @@ fmgr_security_definer(PG_FUNCTION_ARGS)
 
 	fcinfo->flinfo = save_flinfo;
 
+	if (fcache->proconfig)
+		AtEOXact_GUC(true, save_nestlevel);
 	if (set_sql_dialect)
 	{
 
@@ -892,8 +894,6 @@ fmgr_security_definer(PG_FUNCTION_ARGS)
 		if (sql_dialect_value == pg_dialect)
 			non_tsql_proc_entry_hook(non_tsql_proc_count * -1, sys_func_count * -1);
 	}
-	else if (fcache->proconfig)
-		AtEOXact_GUC(true, save_nestlevel);
 	if (OidIsValid(fcache->userid))
 		SetUserIdAndSecContext(save_userid, save_sec_context);
 	if (fmgr_hook)
