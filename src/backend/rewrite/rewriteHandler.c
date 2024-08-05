71048e3b30aff29ba18138076b66c4ad6a81106b
Masahiko Sawada <msawada@postgresql.org>
2024-08-05 06:05:28 -0700

    Restrict accesses to non-system views and foreign tables during pg_dump.
    When pg_dump retrieves the list of database objects and performs the
    data dump, there was possibility that objects are replaced with others
    of the same name, such as views, and access them. This vulnerability
    could result in code execution with superuser privileges during the
    pg_dump process.
    
    This issue can arise when dumping data of sequences, foreign
    tables (only 13 or later), or tables registered with a WHERE clause in
    the extension configuration table.
    
    To address this, pg_dump now utilizes the newly introduced
    restrict_nonsystem_relation_kind GUC parameter to restrict the
    accesses to non-system views and foreign tables during the dump
    process. This new GUC parameter is added to back branches too, but
    these changes do not require cluster recreation.
    
    Back-patch to all supported branches.
    
    Reviewed-by: Noah Misch
    Security: CVE-2024-7348
    Backpatch-through: 12
    (cherry picked from commit 6aba85a4b0a0e60126cc7c598b3e010e272516ec)

--- src/backend/rewrite/rewriteHandler.c
+++ src/backend/rewrite/rewriteHandler.c
@@ -42,6 +42,7 @@
 #include "rewrite/rewriteManip.h"
 #include "rewrite/rewriteSearchCycle.h"
 #include "rewrite/rowsecurity.h"
+#include "tcop/tcopprot.h"
 #include "utils/builtins.h"
 #include "utils/lsyscache.h"
 #include "utils/rel.h"
@@ -1743,6 +1744,14 @@ ApplyRetrieveRule(Query *parsetree,
 	if (rule->qual != NULL)
 		elog(ERROR, "cannot handle qualified ON SELECT rule");
 
+	/* Check if the expansion of non-system views are restricted */
+	if (unlikely((restrict_nonsystem_relation_kind & RESTRICT_RELKIND_VIEW) != 0 &&
+				 RelationGetRelid(relation) >= FirstNormalObjectId))
+		ereport(ERROR,
+				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
+				 errmsg("access to non-system view \"%s\" is restricted",
+						RelationGetRelationName(relation))));
+
 	if (rt_index == parsetree->resultRelation)
 	{
 		/*
@@ -3106,6 +3115,14 @@ rewriteTargetView(Query *parsetree, Relation view)
 				break;
 		}
 	}
+
+	/* Check if the expansion of non-system views are restricted */
+	if (unlikely((restrict_nonsystem_relation_kind & RESTRICT_RELKIND_VIEW) != 0 &&
+				 RelationGetRelid(view) >= FirstNormalObjectId))
+		ereport(ERROR,
+				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
+				 errmsg("access to non-system view \"%s\" is restricted",
+						RelationGetRelationName(view))));
 
 	/*
 	 * For INSERT/UPDATE the modified columns must all be updatable. Note that
