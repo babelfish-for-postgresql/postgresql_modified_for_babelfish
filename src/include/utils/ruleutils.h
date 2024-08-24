35a6374f1c1c3e1829b73fd7ec16f418de65d992
Alexander Korotkov <akorotkov@postgresql.org>
2024-08-24 18:48:48 +0300

    Revert support for ALTER TABLE ... MERGE/SPLIT PARTITION(S) commands
    BABELFISH-CONFLICT: see Postgres community repo for original commit
    
    This commit reverts 1adf16b8fb, 87c21bb941, and subsequent fixes and
    improvements including df64c81ca9, c99ef1811a, 9dfcac8e15, 885742b9f8,
    842c9b2705, fcf80c5d5f, 96c7381c4c, f4fc7cb54b, 60ae37a8bc, 259c96fa8f,
    449cdcd486, 3ca43dbbb6, 2a679ae94e, 3a82c689fd, fbd4321fd5, d53a4286d7,
    c086896625, 4e5d6c4091, 04158e7fa3.
    
    The reason for reverting is security issues related to repeatable name lookups
    (CVE-2014-0062).  Even though 04158e7fa3 solved part of the problem, there
    are still remaining issues, which aren't feasible to even carefully analyze
    before the RC deadline.
    
    Reported-by: Noah Misch, Robert Haas
    Discussion: https://postgr.es/m/20240808171351.a9.nmisch%40google.com
    Backpatch-through: 17
    (cherry picked from commit 84f594da358861cceeaeb7a97bb58f3765eeb284)

--- src/include/utils/ruleutils.h
+++ src/include/utils/ruleutils.h
@@ -54,7 +54,4 @@ typedef int (*print_pltsql_function_arguments_hook_type) (StringInfo buf,
 														  bool print_table_args,
 														  bool print_defaults);
 extern PGDLLEXPORT print_pltsql_function_arguments_hook_type print_pltsql_function_arguments_hook;
-
-extern char *get_list_partvalue_string(Const *val);
-
 #endif							/* RULEUTILS_H */
