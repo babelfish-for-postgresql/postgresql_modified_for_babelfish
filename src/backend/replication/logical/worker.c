30205b9944c76f83b7f753e9b2932ca12ad86583
Amit Kapila <akapila@postgresql.org>
2024-08-29 09:12:12 +0530

    Rename the conflict types for the origin differ cases.
    The conflict types 'update_differ' and 'delete_differ' indicate that a row
    to be modified was previously altered by another origin. Rename those to
    'update_origin_differs' and 'delete_origin_differs' to clarify their
    meaning.
    
    Author: Hou Zhijie
    Reviewed-by: Shveta Malik, Peter Smith
    Discussion: https://postgr.es/m/CAA4eK1+HEKwG_UYt4Zvwh5o_HoCKCjEGesRjJX38xAH3OxuuYA@mail.gmail.com
    (cherry picked from commit 640178c92e3f6c74d275369b291b507834f1309e)

--- src/backend/replication/logical/worker.c
+++ src/backend/replication/logical/worker.c
@@ -2716,7 +2716,7 @@ apply_handle_update_internal(ApplyExecutionData *edata,
 			newslot = table_slot_create(localrel, &estate->es_tupleTable);
 			slot_store_data(newslot, relmapentry, newtup);
 
-			ReportApplyConflict(estate, relinfo, LOG, CT_UPDATE_DIFFER,
+			ReportApplyConflict(estate, relinfo, LOG, CT_UPDATE_ORIGIN_DIFFERS,
 								remoteslot, localslot, newslot,
 								InvalidOid, localxmin, localorigin, localts);
 		}
@@ -2885,7 +2885,7 @@ apply_handle_delete_internal(ApplyExecutionData *edata,
 		 */
 		if (GetTupleTransactionInfo(localslot, &localxmin, &localorigin, &localts) &&
 			localorigin != replorigin_session_origin)
-			ReportApplyConflict(estate, relinfo, LOG, CT_DELETE_DIFFER,
+			ReportApplyConflict(estate, relinfo, LOG, CT_DELETE_ORIGIN_DIFFERS,
 								remoteslot, localslot, NULL,
 								InvalidOid, localxmin, localorigin, localts);
 
@@ -3114,7 +3114,7 @@ apply_handle_tuple_routing(ApplyExecutionData *edata,
 					newslot = table_slot_create(partrel, &estate->es_tupleTable);
 					slot_store_data(newslot, part_entry, newtup);
 
-					ReportApplyConflict(estate, partrelinfo, LOG, CT_UPDATE_DIFFER,
+					ReportApplyConflict(estate, partrelinfo, LOG, CT_UPDATE_ORIGIN_DIFFERS,
 										remoteslot_part, localslot, newslot,
 										InvalidOid, localxmin, localorigin,
 										localts);
