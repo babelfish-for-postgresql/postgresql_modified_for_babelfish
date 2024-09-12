dfb3b5716b5cd143c3c5a0247f6ce1d219ad7bc9
Shameem Ahmed <shameemahmed20apr2000@gmail.com>
2024-09-12 08:29:09 +0000

    Update logic
    Signed-off-by: Shameem Ahmed <shameemahmed20apr2000@gmail.com>

--- src/bin/pg_dump/dump_babel_utils.c
+++ src/bin/pg_dump/dump_babel_utils.c
@@ -9,7 +9,6 @@
  *
  *-------------------------------------------------------------------------
  */
-#include "postgres.h"
 #include "postgres_fe.h"
 
 #include "catalog/pg_class_d.h"
@@ -512,13 +511,29 @@ fixTsqlDefaultExpr(Archive *fout, AttrDefInfo *attrDefInfo)
 		return;
 
 	atttypname = attrDefInfo->adtable->atttypnames[attrDefInfo->adnum - 1];
-	if (strstr(atttypname, "varchar") != NULL || strstr(atttypname, "sys.varchar") != NULL)
+	if (strstr(source, "COLLATE") != NULL)
 	{
-		char *collate_ptr = strstr(attrDefInfo->adef_expr, " COLLATE ");
-		if (collate_ptr != NULL)
-			*collate_ptr = '\0';
+		size_t input_len, output_len;
+		char *output_str = NULL;
+
+		input_len = strlen(attrDefInfo->adef_expr);
+		output_len = input_len + 3;  // 2 parentheses + null terminator
+		output_str = (char *)malloc(output_len * sizeof(char));
+
+		// Add the opening parenthesis
+		output_str[0] = '(';
+
+		// Copy the input string to the output string
+		strncpy(output_str + 1, attrDefInfo->adef_expr, input_len);
+
+		// Add the closing parenthesis and null terminator
+		output_str[output_len - 2] = ')';
+		output_str[output_len - 1] = '\0';
+
+		strcpy(attrDefInfo->adef_expr, output_str);
+		free(output_str);
 		free(source);
-		return; 
+		return;
 	}
 
 	if (!strstr(atttypname, "decimal") && !strstr(atttypname, "numeric"))
