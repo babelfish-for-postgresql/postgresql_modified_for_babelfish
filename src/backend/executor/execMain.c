3670574cd120336a65c7c3537dd896a0612c982e
Noah Misch <noah@leadboat.com>
2024-09-24 15:25:18 -0700

    For inplace update durability, make heap_update() callers wait.
    BABELFISH-CONFLICT: see Postgres community repo for original commit
    
    The previous commit fixed some ways of losing an inplace update.  It
    remained possible to lose one when a backend working toward a
    heap_update() copied a tuple into memory just before inplace update of
    that tuple.  In catalogs eligible for inplace update, use LOCKTAG_TUPLE
    to govern admission to the steps of copying an old tuple, modifying it,
    and issuing heap_update().  This includes MERGE commands.  To avoid
    changing most of the pg_class DDL, don't require LOCKTAG_TUPLE when
    holding a relation lock sufficient to exclude inplace updaters.
    Back-patch to v12 (all supported versions).  In v13 and v12, "UPDATE
    pg_class" or "UPDATE pg_database" can still lose an inplace update.  The
    v14+ UPDATE fix needs commit 86dc90056dfdbd9d1b891718d2e5614e3e432f35,
    and it wasn't worth reimplementing that fix without such infrastructure.
    
    Reviewed by Nitin Motiani and (in earlier versions) Heikki Linnakangas.
    
    Discussion: https://postgr.es/m/20231027214946.79.nmisch@google.com
    (cherry picked from commit 51ff46de29f67d73549b2858f57e77ada8513369)

--- src/backend/executor/execMain.c
+++ src/backend/executor/execMain.c
@@ -1033,6 +1033,10 @@ CheckValidResultRel(ResultRelInfo *resultRelInfo, CmdType operation)
 	TriggerDesc *trigDesc = resultRel->trigdesc;
 	FdwRoutine *fdwroutine;
 
+	/* Expect a fully-formed ResultRelInfo from InitResultRelInfo(). */
+	Assert(resultRelInfo->ri_needLockTagTuple ==
+		   IsInplaceUpdateRelation(resultRel));
+
 	switch (resultRel->rd_rel->relkind)
 	{
 		case RELKIND_RELATION:
@@ -1241,6 +1245,8 @@ InitResultRelInfo(ResultRelInfo *resultRelInfo,
 	resultRelInfo->ri_NumIndices = 0;
 	resultRelInfo->ri_IndexRelationDescs = NULL;
 	resultRelInfo->ri_IndexRelationInfo = NULL;
+	resultRelInfo->ri_needLockTagTuple =
+		IsInplaceUpdateRelation(resultRelationDesc);
 	/* make a copy so as not to depend on relcache info not changing... */
 	resultRelInfo->ri_TrigDesc = CopyTriggerDesc(resultRelationDesc->trigdesc);
 	if (resultRelInfo->ri_TrigDesc)
