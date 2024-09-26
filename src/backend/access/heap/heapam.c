5e1be6dca12e8c511a199f0ef7b91ab93fa4d45d
Mihir Jadhav <msj@amazon.com>
2024-09-26 20:43:27 +0000

    BABEL: Fixes after community commit 7abe267
    Signed-off-by: Mihir Jadhav <msj@amazon.com>

--- src/backend/access/heap/heapam.c
+++ src/backend/access/heap/heapam.c
@@ -6293,7 +6293,7 @@ heap_inplace_lock(Relation relation,
 	 * - don't try to continue even if the updater aborts: likewise
 	 * - no crosscheck
 	 */
-	result = HeapTupleSatisfiesUpdate(&oldtup, GetCurrentCommandId(false),
+	result = HeapTupleSatisfiesUpdate(relation, &oldtup, GetCurrentCommandId(false),
 									  buffer);
 
 	if (result == TM_Invisible)
