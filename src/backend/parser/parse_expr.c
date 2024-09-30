188fd3743dde3041a91d9f79691ff629bda4b779
yashneet vinayak <yashneet@amazon.com>
2024-09-30 08:28:55 +0000

    adding space between if and '('
    Signed-off-by: yashneet vinayak <yashneet@amazon.com>

--- src/backend/parser/parse_expr.c
+++ src/backend/parser/parse_expr.c
@@ -1836,7 +1836,7 @@ transformCaseExpr(ParseState *pstate, CaseExpr *c)
 	newc->location = c->location;
 
 	/* Following hook will be used to set the typmod of all the CASE Branches. */
-	if(sql_dialect == SQL_DIALECT_TSQL && set_common_typmod_case_expr_hook)
+	if (sql_dialect == SQL_DIALECT_TSQL && set_common_typmod_case_expr_hook)
 		(*set_common_typmod_case_expr_hook)(pstate, resultexprs, newc);
 
 	return (Node *) newc;
