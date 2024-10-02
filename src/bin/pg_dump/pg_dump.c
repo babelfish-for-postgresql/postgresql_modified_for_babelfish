0fbfe464e6f881f13aeeee263e53b5510491287b
Daniel Gustafsson <dgustafsson@postgresql.org>
2024-10-02 13:08:55 +0200

    Add fastpaths for when no objects are found
    If there are no objects found, there is no reason to inspect the
    result columns and mallocing a zero-sized  (which will be 1 byte
    in reality) heap buffer for it.  Add a fast-path for immediately
    returning like how other object inspection functions are already
    doing it.
    
    Reviewed-by: Ranier Vilela <ranier.vf@gmail.com>
    Discussion: https://postgr.es/m/C2F05B3C-1414-45DD-AE09-6FEE4D0F89BD@yesql.se
    (cherry picked from commit 9c733951049bf3993c886d7f2c7459e7439a9793)

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -4387,6 +4387,9 @@ getPublications(Archive *fout)
 
 	ntups = PQntuples(res);
 
+	if (ntups == 0)
+		goto cleanup;
+
 	i_tableoid = PQfnumber(res, "tableoid");
 	i_oid = PQfnumber(res, "oid");
 	i_pubname = PQfnumber(res, "pubname");
@@ -4425,6 +4428,8 @@ getPublications(Archive *fout)
 		/* Decide whether we want to dump it */
 		selectDumpableObject(&(pubinfo[i].dobj), fout);
 	}
+
+cleanup:
 	PQclear(res);
 
 	destroyPQExpBuffer(query);
@@ -5891,7 +5896,7 @@ getExtensions(Archive *fout, int *numExtensions)
 	int			ntups;
 	int			i;
 	PQExpBuffer query;
-	ExtensionInfo *extinfo;
+	ExtensionInfo *extinfo = NULL;
 	int			i_tableoid;
 	int			i_oid;
 	int			i_extname;
@@ -5911,6 +5916,8 @@ getExtensions(Archive *fout, int *numExtensions)
 	res = ExecuteSqlQuery(fout, query->data, PGRES_TUPLES_OK);
 
 	ntups = PQntuples(res);
+	if (ntups == 0)
+		goto cleanup;
 
 	extinfo = (ExtensionInfo *) pg_malloc(ntups * sizeof(ExtensionInfo));
 
@@ -5941,6 +5948,7 @@ getExtensions(Archive *fout, int *numExtensions)
 		bbf_selectDumpableObject((DumpableObject *)&(extinfo[i]), fout);
 	}
 
+cleanup:
 	PQclear(res);
 	destroyPQExpBuffer(query);
 
