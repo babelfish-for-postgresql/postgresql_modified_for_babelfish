4e4d9577ad0cbfe240de319d24a98b80bb012490
Michael Paquier <michael@paquier.xyz>
2024-10-03 10:55:02 +0900

    Remove support for unlogged on partitioned tables
    The following commands were allowed on partitioned tables, with
    different effects:
    1) ALTER TABLE SET [UN]LOGGED did not issue an error, and did not update
    pg_class.relpersistence.
    2) CREATE UNLOGGED TABLE was working with pg_class.relpersistence marked
    as initially defined, but partitions did not inherit the UNLOGGED
    property, which was confusing.
    
    This commit causes the commands mentioned above to fail for partitioned
    tables, instead.
    
    pg_dump is tweaked so as partitioned tables marked as UNLOGGED ignore
    the option when dumped from older server versions.  pgbench needs a
    tweak for --unlogged and --partitions=N to ignore the UNLOGGED option on
    the partitioned tables created, its partitions still being unlogged.
    
    Author: Michael Paquier
    Reviewed-by: Nathan Bossart
    Discussion: https://postgr.es/m/ZiiyGFTBNkqcMQi_@paquier.xyz
    (cherry picked from commit e2bab2d792044b55dd092bf1c2be0d570ccb9401)

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -16028,8 +16028,13 @@ dumpTableSchema(Archive *fout, const TableInfo *tbinfo)
 			appendPQExpBufferStr(q,
 								 "SET babelfishpg_tsql.restore_tsql_tabletype = TRUE;\n");
 
+		/*
+		 * PostgreSQL 18 has disabled UNLOGGED for partitioned tables, so
+		 * ignore it when dumping if it was set in this case.
+		 */
 		appendPQExpBuffer(q, "CREATE %s%s %s",
-						  tbinfo->relpersistence == RELPERSISTENCE_UNLOGGED ?
+						  (tbinfo->relpersistence == RELPERSISTENCE_UNLOGGED &&
+						   tbinfo->relkind != RELKIND_PARTITIONED_TABLE) ?
 						  "UNLOGGED " : "",
 						  reltypename,
 						  qualrelname);
