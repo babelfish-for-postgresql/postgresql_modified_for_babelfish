27c8ec6b2f2f63c1f76f07f65a993024e78313ed
Jason Teng <jasonten@amazon.com>
2024-10-08 21:04:07 +0000

    BABEL: Skip locking the tuple for ENR-related tuple modifications.
    Commit 09f0820095ea8b7c6ca4269454a94695c8421628 introduced
    locking for individual tuples during certain operations. However,
    Babelfish introduced the concept of ENR-only relations, which
    store all catalog tuples in their own local cache and not in the
    physical catalogs. As these relations and their catalog tuples
    are completely session-local, there is no need to acquire locks on
    tuples for these relations (and it would just lead to a crash
    anyways since the tuples do not have an underlying TID to lock against).
    
    Signed-off-by: Jason Teng <jasonten@amazon.com>
    (cherry picked from commit e2f0b17924d7853dc4b2f56cb59bfb1d23a0a86f)

--- src/backend/utils/cache/relcache.c
+++ src/backend/utils/cache/relcache.c
@@ -73,6 +73,8 @@
 #include "nodes/makefuncs.h"
 #include "nodes/nodeFuncs.h"
 #include "optimizer/optimizer.h"
+#include "parser/parser.h"
+#include "postmaster/autovacuum.h"
 #include "pgstat.h"
 #include "rewrite/rewriteDefine.h"
 #include "rewrite/rowsecurity.h"
@@ -3718,6 +3720,7 @@ RelationSetNewRelfilenumber(Relation relation, char persistence)
 	MultiXactId minmulti = InvalidMultiXactId;
 	TransactionId freezeXid = InvalidTransactionId;
 	RelFileLocator newrlocator;
+	bool			is_enr = false;
 
 	if (!IsBinaryUpgrade)
 	{
@@ -3755,8 +3758,13 @@ RelationSetNewRelfilenumber(Relation relation, char persistence)
 	 */
 	pg_class = table_open(RelationRelationId, RowExclusiveLock);
 
-	tuple = SearchSysCacheLockedCopy1(RELOID,
-									  ObjectIdGetDatum(RelationGetRelid(relation)));
+	is_enr = (sql_dialect == SQL_DIALECT_TSQL && get_ENR_withoid(currentQueryEnv, RelationGetRelid(relation), ENR_TSQL_TEMP));
+	if (is_enr)
+		tuple = SearchSysCacheCopy1(RELOID,
+									ObjectIdGetDatum(RelationGetRelid(relation)));
+	else
+		tuple = SearchSysCacheLockedCopy1(RELOID,
+										  ObjectIdGetDatum(RelationGetRelid(relation)));
 	if (!HeapTupleIsValid(tuple))
 		elog(ERROR, "could not find tuple for relation %u",
 			 RelationGetRelid(relation));
@@ -3883,7 +3891,8 @@ RelationSetNewRelfilenumber(Relation relation, char persistence)
 		CatalogTupleUpdate(pg_class, &otid, tuple);
 	}
 
-	UnlockTuple(pg_class, &otid, InplaceUpdateTupleLock);
+	if (!is_enr)
+		UnlockTuple(pg_class, &otid, InplaceUpdateTupleLock);
 	heap_freetuple(tuple);
 
 	table_close(pg_class, RowExclusiveLock);
