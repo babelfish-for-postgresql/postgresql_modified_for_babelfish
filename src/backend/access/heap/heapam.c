61b87e1531b14f8a4ab0ba3a2e8ccf33f5482328
Noah Misch <noah@leadboat.com>
2024-10-29 09:39:55 -0700

    Unpin buffer before inplace update waits for an XID to end.
    Commit a07e03fd8fa7daf4d1356f7cb501ffe784ea6257 changed inplace updates
    to wait for heap_update() commands like GRANT TABLE and GRANT DATABASE.
    By keeping the pin during that wait, a sequence of autovacuum workers
    and an uncommitted GRANT starved one foreground LockBufferForCleanup()
    for six minutes, on buildfarm member sarus.  Prevent, at the cost of a
    bit of complexity.  Back-patch to v12, like the earlier commit.  That
    commit and heap_inplace_lock() have not yet appeared in any release.
    
    Discussion: https://postgr.es/m/20241026184936.ae.nmisch@google.com
    (cherry picked from commit 30d47ec8c6c78f46bcd7116b7effb6988b126e43)

--- src/backend/access/heap/heapam.c
+++ src/backend/access/heap/heapam.c
@@ -6172,8 +6172,8 @@ heap_abort_speculative(Relation relation, ItemPointer tid)
  * transaction.  If compatible, return true with the buffer exclusive-locked,
  * and the caller must release that by calling
  * heap_inplace_update_and_unlock(), calling heap_inplace_unlock(), or raising
- * an error.  Otherwise, return false after blocking transactions, if any,
- * have ended.
+ * an error.  Otherwise, call release_callback(arg), wait for blocking
+ * transactions to end, and return false.
  *
  * Since this is intended for system catalogs and SERIALIZABLE doesn't cover
  * DDL, this doesn't guarantee any particular predicate locking.
@@ -6207,7 +6207,8 @@ heap_abort_speculative(Relation relation, ItemPointer tid)
  */
 bool
 heap_inplace_lock(Relation relation,
-				  HeapTuple oldtup_ptr, Buffer buffer)
+				  HeapTuple oldtup_ptr, Buffer buffer,
+				  void (*release_callback) (void *), void *arg)
 {
 	HeapTupleData oldtup = *oldtup_ptr; /* minimize diff vs. heap_update() */
 	TM_Result	result;
@@ -6272,6 +6273,7 @@ heap_inplace_lock(Relation relation,
 										lockmode, NULL))
 			{
 				LockBuffer(buffer, BUFFER_LOCK_UNLOCK);
+				release_callback(arg);
 				ret = false;
 				MultiXactIdWait((MultiXactId) xwait, mxact_status, infomask,
 								relation, &oldtup.t_self, XLTW_Update,
@@ -6287,6 +6289,7 @@ heap_inplace_lock(Relation relation,
 		else
 		{
 			LockBuffer(buffer, BUFFER_LOCK_UNLOCK);
+			release_callback(arg);
 			ret = false;
 			XactLockTableWait(xwait, relation, &oldtup.t_self,
 							  XLTW_Update);
@@ -6298,6 +6301,7 @@ heap_inplace_lock(Relation relation,
 		if (!ret)
 		{
 			LockBuffer(buffer, BUFFER_LOCK_UNLOCK);
+			release_callback(arg);
 		}
 	}
 
