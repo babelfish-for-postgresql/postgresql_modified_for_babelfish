2f7d83a4c15c4860dcaf64c954be1c2fd24ccefa
Peter Eisentraut <peter@eisentraut.org>
2024-12-04 16:43:07 +0100

    Fix dead code
    from commit 85b7efa1cdd
    
    per Coverity report
    
    (cherry picked from commit dfbb092cff70ee4d4084b7e4e57228a89ff4f9f8)

--- src/backend/utils/adt/like_match.c
+++ src/backend/utils/adt/like_match.c
@@ -412,13 +412,14 @@ MatchText(const char *t, int tlen, const char *p, int plen, pg_locale_t locale)
 				 * fails.  Otherwise, try again with a longer substring.
 				 */
 				if (t1len == 0)
+				{
+					if (buf)
+						pfree(buf);
 					return LIKE_FALSE;
+				}
 				else
 					NextChar(t1, t1len);
 			}
-			if (buf)
-				pfree(buf);
-			continue;
 		}
 		else if (GETCHAR(*p, locale) != GETCHAR(*t, locale))
 		{
