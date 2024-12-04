65a7163012566768f3e29265a018cbfb790e25e1
Bo <hopelee1994@gmail.com>
2024-12-03 16:20:33 -0800

    ExecProcessReturning is unexpectedly called twice for psql request causing overflow error (#488)
    In community postgresql, ExecProcessReturning is called once after the
    main process of ExecInsert. But due to some merging errors related to
    Babelfish and T-SQL, this function is wrongly called twice for psql
    requests. At the first call, the xmax is not initialized causing
    unexpected error raising.
    
    The first intention of these pieces of codes is to call it in the
    first place only for T-SQL(CR-52326143). And then got wrongly
    cherry-picked in CR-57250041. Finally, the condition check gets
    removed in CR-65026879. We should keep the first intention of the
    codes, which also makes it consistent with the behaviour in
    ExecUpdate.
    
    Task: BABEL-5343
    Signed-off-by: Bo Li <zboli@amazon.com>
    (cherry picked from commit db4e1389f2af4e4b2ec9e56e3520cabadb939a16)

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -823,7 +823,7 @@ ExecInsert(ModifyTableContext *context,
 	}
 
 	/* Process RETURNING if present */
-	if (resultRelInfo->ri_projectReturning)
+	if (resultRelInfo->ri_projectReturning && sql_dialect == SQL_DIALECT_TSQL)
 		result = ExecProcessReturning(resultRelInfo, slot, planSlot);
 
 	/* INSTEAD OF ROW INSERT Triggers */
@@ -1217,7 +1217,7 @@ ExecInsert(ModifyTableContext *context,
 		ExecWithCheckOptions(WCO_VIEW_CHECK, resultRelInfo, slot, estate);
 
 	/* Process RETURNING if present */
-	if (resultRelInfo->ri_projectReturning)
+	if (resultRelInfo->ri_projectReturning && sql_dialect != SQL_DIALECT_TSQL)
 		result = ExecProcessReturning(resultRelInfo, slot, planSlot);
 
 	if (inserted_tuple)
