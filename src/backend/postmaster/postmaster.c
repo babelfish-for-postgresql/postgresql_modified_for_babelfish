43ef2337e4b78f34d30b9de17e43852832372ef2
Noah Misch <noah@leadboat.com>
2024-12-10 13:51:59 -0800

    Fix elog(FATAL) before PostmasterMain() or just after fork().
    Since commit 97550c0711972a9856b5db751539bbaf2f88884c, these failed with
    "PANIC:  proc_exit() called in child process" due to uninitialized or
    stale MyProcPid.  That was reachable if close() failed in
    ClosePostmasterPorts() or setlocale(category, "C") failed, both
    unlikely.  Back-patch to v13 (all supported versions).
    
    Discussion: https://postgr.es/m/20241208034614.45.nmisch@google.com
    (cherry picked from commit ac4a2b40392bd5ca9e9e02bad50d437411a8adfb)

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -2813,14 +2813,13 @@ ClosePostmasterPorts(bool am_syslogger)
 
 
 /*
- * InitProcessGlobals -- set MyProcPid, MyStartTime[stamp], random seeds
+ * InitProcessGlobals -- set MyStartTime[stamp], random seeds
  *
  * Called early in the postmaster and every backend.
  */
 void
 InitProcessGlobals(void)
 {
-	MyProcPid = getpid();
 	MyStartTimestamp = GetCurrentTimestamp();
 	MyStartTime = timestamptz_to_time_t(MyStartTimestamp);
 
