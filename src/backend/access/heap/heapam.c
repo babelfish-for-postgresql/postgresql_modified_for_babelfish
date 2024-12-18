770820c89c5265aa6f41e411a191d0f625456d2f
Melanie Plageman <melanieplageman@gmail.com>
2024-12-18 11:47:38 -0500

    Make rs_cindex and rs_ntuples unsigned
    HeapScanDescData.rs_cindex and rs_ntuples can't be less than 0. All scan
    types using the heap scan descriptor expect these values to be >= 0.
    Make that expectation clear by making rs_cindex and rs_ntuples unsigned.
    
    Also remove the test in heapam_scan_bitmap_next_tuple() that checks if
    rs_cindex < 0. This was never true, but now that rs_cindex is unsigned,
    it makes even less sense.
    
    While we are at it, initialize both rs_cindex and rs_ntuples to 0 in
    initscan().
    
    Author: Melanie Plageman
    Reviewed-by: Dilip Kumar
    Discussion: https://postgr.es/m/CAAKRu_ZxF8cDCM_BFi_L-t%3DRjdCZYP1usd1Gd45mjHfZxm0nZw%40mail.gmail.com
    (cherry picked from commit 68d9662be1c4b705123a0e292974fb4be661294c)

--- src/backend/access/heap/heapam.c
+++ src/backend/access/heap/heapam.c
@@ -379,6 +379,8 @@ initscan(HeapScanDesc scan, ScanKey key, bool keep_startblock)
 	ItemPointerSetInvalid(&scan->rs_ctup.t_self);
 	scan->rs_cbuf = InvalidBuffer;
 	scan->rs_cblock = InvalidBlockNumber;
+	scan->rs_ntuples = 0;
+	scan->rs_cindex = 0;
 
 	/*
 	 * Initialize to ForwardScanDirection because it is most common and
@@ -945,8 +947,8 @@ heapgettup_pagemode(HeapScanDesc scan,
 {
 	HeapTuple	tuple = &(scan->rs_ctup);
 	Page		page;
-	int			lineindex;
-	int			linesleft;
+	uint32		lineindex;
+	uint32		linesleft;
 
 	if (likely(scan->rs_inited))
 	{
@@ -991,6 +993,7 @@ continue_page:
 			ItemId		lpp;
 			OffsetNumber lineoff;
 
+			Assert(lineindex <= scan->rs_ntuples);
 			lineoff = scan->rs_vistuples[lineindex];
 			lpp = PageGetItemId(page, lineoff);
 			Assert(ItemIdIsNormal(lpp));
