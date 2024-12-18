5ce830841b37035258f0766e8a1cfc3f52396c0d
Melanie Plageman <melanieplageman@gmail.com>
2024-12-18 18:19:28 -0500

    Add common interface for TBMIterators
    Add and use TBMPrivateIterator, which replaces the current TBMIterator
    for serial use cases, and repurpose TBMIterator to be a unified
    interface for both the serial ("private") and parallel ("shared") TID
    Bitmap iterator interfaces. This encapsulation simplifies call sites for
    callers supporting both parallel and serial TID Bitmap access.
    TBMIterator is not yet used in this commit.
    
    Author: Melanie Plageman
    Reviewed-by: Tomas Vondra, Heikki Linnakangas
    Discussion: https://postgr.es/m/063e4eb4-32d9-439e-a0b1-75565a9835a8%40iki.fi
    (cherry picked from commit 7f9d4187e7bab10329cc00aff34cfba08248d4be)

--- src/include/nodes/execnodes.h
+++ src/include/nodes/execnodes.h
@@ -1869,7 +1869,7 @@ typedef struct BitmapHeapScanState
 	TIDBitmap  *tbm;
 	Buffer		pvmbuffer;
 	BitmapHeapScanInstrumentation stats;
-	TBMIterator *prefetch_iterator;
+	TBMPrivateIterator *prefetch_iterator;
 	int			prefetch_pages;
 	int			prefetch_target;
 	int			prefetch_maximum;
