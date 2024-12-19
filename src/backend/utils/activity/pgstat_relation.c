751378faa4b7463a99e56513f3ba275a83fed6b8
Michael Paquier <michael@paquier.xyz>
2024-12-19 13:19:22 +0900

    Add backend-level statistics to pgstats
    This adds a new variable-numbered statistics kind in pgstats, where the
    object ID key of the stats entries is based on the proc number of the
    backends.  This acts as an upper-bound for the number of stats entries
    that can exist at once.  The entries are created when a backend starts
    after authentication succeeds, and are removed when the backend exits,
    making the stats entry exist for as long as their backend is up and
    running.  These are not written to the pgstats file at shutdown (note
    that write_to_file is disabled, as a safety measure).
    
    Currently, these stats include only information about the I/O generated
    by a backend, using the same layer as pg_stat_io, except that it is now
    possible to know how much activity is happening in each backend rather
    than an overall aggregate of all the activity.  A function called
    pg_stat_get_backend_io() is added to access this data depending on the
    PID of a backend.  The existing structure could be expanded in the
    future to add more information about other statistics related to
    backends, depending on requirements or ideas.
    
    Auxiliary processes are not included in this set of statistics.  These
    are less interesting to have than normal backends as they have dedicated
    entries in pg_stat_io, and stats kinds of their own.
    
    This commit includes also pg_stat_reset_backend_stats(), function able
    to reset all the stats associated to a single backend.
    
    Bump catalog version and PGSTAT_FILE_FORMAT_ID.
    
    Author: Bertrand Drouvot
    Reviewed-by: Álvaro Herrera, Kyotaro Horiguchi, Michael Paquier, Nazir
    Bilal Yavuz
    Discussion: https://postgr.es/m/ZtXR+CtkEVVE/LHF@ip-10-97-1-34.eu-west-3.compute.internal
    
    (cherry picked from commit 9aea73fc61d4e77e000724ce0b2f896590a10e03)

--- src/backend/utils/activity/pgstat_relation.c
+++ src/backend/utils/activity/pgstat_relation.c
@@ -273,6 +273,7 @@ pgstat_report_vacuum(Oid tableoid, bool shared,
 	 * VACUUM command has processed all tables and committed.
 	 */
 	pgstat_flush_io(false);
+	pgstat_flush_backend(false);
 }
 
 /*
@@ -359,6 +360,7 @@ pgstat_report_analyze(Relation rel,
 
 	/* see pgstat_report_vacuum() */
 	pgstat_flush_io(false);
+	pgstat_flush_backend(false);
 }
 
 /*
