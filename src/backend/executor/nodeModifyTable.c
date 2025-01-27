dbd186bcb1235c5b9b58c8e452a0612d9372f1f3
Tanzeel Khan <140405735+tanscorpio7@users.noreply.github.com>
2025-01-27 22:31:24 +0530

    [BABEL] Fix warnings in IsTsqlTableVariable & ExecModifyTable  (#520)
    1. Fix warning in IsTsqlTableVariable
    2. Supress warning for uninitialized variable `tss`
    3. Configure engine without readline in github actions
    
    #### Engine Change https://github.com/babelfish-for-postgresql/postgresql_modified_for_babelfish/pull/520
    #### Extensios Change https://github.com/babelfish-for-postgresql/babelfish_extensions/pull/3430
    
    Signed-off-by: Tanzeel Khan <tzlkhan@amazon.com>
    (cherry picked from commit 84b4e98082830e6be010866e8b426dbfe9a268cd)

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -3895,8 +3895,8 @@ ExecModifyTable(PlanState *pstate)
 	ItemPointer tupleid;
 	/* for INSERT ... EXECUTE */
 	bool 		tsql_insert_exec = node->callStmt != NULL;
-	Tuplestorestate *tss;
-	TupleDesc 	tupdesc;
+	Tuplestorestate *tss = NULL;
+	TupleDesc 	tupdesc = NULL;
 	DestReceiver *dest = NULL;
 
 	CHECK_FOR_INTERRUPTS();
