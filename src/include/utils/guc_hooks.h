3b28f3f937857cb401398df1194a97c5e474e887
Amit Kapila <akapila@postgresql.org>
2025-02-19 09:29:50 +0530

    Invalidate inactive replication slots.
    This commit introduces idle_replication_slot_timeout GUC that allows
    inactive slots to be invalidated at the time of checkpoint. Because
    checkpoints happen checkpoint_timeout intervals, there can be some lag
    between when the idle_replication_slot_timeout was exceeded and when the
    slot invalidation is triggered at the next checkpoint. To avoid such lags,
    users can force a checkpoint to promptly invalidate inactive slots.
    
    Note that the idle timeout invalidation mechanism is not applicable for
    slots that do not reserve WAL or for slots on the standby server that are
    synced from the primary server (i.e., standby slots having 'synced' field
    'true'). Synced slots are always considered to be inactive because they
    don't perform logical decoding to produce changes.
    
    The slots can become inactive for a long period if a subscriber is down
    due to a system error or inaccessible because of network issues. If such a
    situation persists, it might be more practical to recreate the subscriber
    rather than attempt to recover the node and wait for it to catch up which
    could be time-consuming.
    
    Then, external tools could create replication slots (e.g., for migrations
    or upgrades) that may fail to remove them if an error occurs, leaving
    behind unused slots that take up space and resources. Manually cleaning
    them up can be tedious and error-prone, and without intervention, these
    lingering slots can cause unnecessary WAL retention and system bloat.
    
    As the duration of idle_replication_slot_timeout is in minutes, any test
    using that would be time-consuming. We are planning to commit a follow up
    patch for tests by using the injection point framework.
    
    Author: Nisha Moond <nisha.moond412@gmail.com>
    Author: Bharath Rupireddy <bharath.rupireddyforpostgres@gmail.com>
    Reviewed-by: Peter Smith <smithpb2250@gmail.com>
    Reviewed-by: Hayato Kuroda <kuroda.hayato@fujitsu.com>
    Reviewed-by: Vignesh C <vignesh21@gmail.com>
    Reviewed-by: Amit Kapila <amit.kapila16@gmail.com>
    Reviewed-by: Hou Zhijie <houzj.fnst@fujitsu.com>
    Reviewed-by: Bertrand Drouvot <bertranddrouvot.pg@gmail.com>
    Discussion: https://postgr.es/m/CALj2ACW4aUe-_uFQOjdWCEN-xXoLGhmvRFnL8SNw_TZ5nJe+aw@mail.gmail.com
    Discussion: https://postgr.es/m/OS0PR01MB5716C131A7D80DAE8CB9E88794FC2@OS0PR01MB5716.jpnprd01.prod.outlook.com
    (cherry picked from commit ac0e33136abc4668c9b08e1ba7db69afe1e0e2c3)

--- src/include/utils/guc_hooks.h
+++ src/include/utils/guc_hooks.h
@@ -177,6 +177,8 @@ extern void assign_wal_sync_method(int new_wal_sync_method, void *extra);
 extern bool check_synchronized_standby_slots(char **newval, void **extra,
 											 GucSource source);
 extern void assign_synchronized_standby_slots(const char *newval, void *extra);
+extern bool check_idle_replication_slot_timeout(int *newval, void **extra,
+												GucSource source);
 
 /* Babelfish-specific hook for updating GUCs */
 typedef void (*guc_newval_hook_type) (const char *guc, bool boolVal, const char *strVal, int intVal);
