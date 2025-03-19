2345233efb0990bf96f366b8c262a4b2822927e8
Peter Eisentraut <peter@eisentraut.org>
2025-03-19 06:57:20 +0100

    extension_control_path
    The new GUC extension_control_path specifies a path to look for
    extension control files.  The default value is $system, which looks in
    the compiled-in location, as before.
    
    The path search uses the same code and works in the same way as
    dynamic_library_path.
    
    Some use cases of this are: (1) testing extensions during package
    builds, (2) installing extensions outside security-restricted
    containers like Python.app (on macOS), (3) adding extensions to
    PostgreSQL running in a Kubernetes environment using operators such as
    CloudNativePG without having to rebuild the base image for each new
    extension.
    
    There is also a tweak in Makefile.global so that it is possible to
    install extensions using PGXS into an different directory than the
    default, using 'make install prefix=/else/where'.  This previously
    only worked when specifying the subdirectories, like 'make install
    datadir=/else/where/share pkglibdir=/else/where/lib', for purely
    implementation reasons.  (Of course, without the path feature,
    installing elsewhere was rarely useful.)
    
    Author: Peter Eisentraut <peter@eisentraut.org>
    Co-authored-by: Matheus Alcantara <matheusssilv97@gmail.com>
    Reviewed-by: David E. Wheeler <david@justatheory.com>
    Reviewed-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Reviewed-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Reviewed-by: Niccolò Fei <niccolo.fei@enterprisedb.com>
    Discussion: https://www.postgresql.org/message-id/flat/E7C7BFFB-8857-48D4-A71F-88B359FADCFD@justatheory.com
    (cherry picked from commit 4f7f7b0375854e2f89876473405a8f21c95012af)

--- src/include/fmgr.h
+++ src/include/fmgr.h
@@ -742,6 +742,9 @@ extern PGFunction lookup_C_func_by_oid(Oid fn_oid, char* probin, char* prosrc);
  */
 extern PGDLLIMPORT char *Dynamic_library_path;
 
+extern char *substitute_path_macro(const char *str, const char *macro, const char *value);
+extern char *find_in_path(const char *basename, const char *path, const char *path_param,
+						  const char *macro, const char *macro_val);
 extern void *load_external_function(const char *filename, const char *funcname,
 									bool signalNotFound, void **filehandle);
 extern void *lookup_external_function(void *filehandle, const char *funcname);
