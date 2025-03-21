55339bff66d53d64a080428e94e3561bab99adb3
Rohit bhagat <65351720+rohit01010@users.noreply.github.com>
2025-03-21 15:57:41 +0530

    Added BBF hook to adjust numeric result to correct scale (#529)
    For Babelfish, the expected behaviour should be for every arithmetic computation which results in numeric datatype, the result should be adjusted(truncated/rounded) to the correct scale. To get this behaviour added hooks at following places to adjust the result value.
    
    evaluate_expr() - All the arithmetic computations that result in constant value, happens here. So added a hook to adjust the result after every such arithmetic computation.
    ExecInterpExpr() - All the arithmetic computations other than mentioned above, happens here. So added a hook to adjust the result after every such arithmetic computation.
    finalize_aggregate() - For aggregate functions, the final result is computed here, so added a hook to adjust the result for aggregate functions.
    Also,
    
    for some nodes such as T_OpExpr, T_DistinctExpr, T_CoerceViaIO the result_typmod was directly hardcoded as -1 in PostgreSQL, Added hook pltsql_exprTypmod_hook to compute the typmod of the expression and passed those instead of -1 to avoid losing typmod information.
    added pltsql_exprTypmod_hook to compute expression typmod for expressions whose typmod is not computed by exprTypmod.
    During initialisation of resulttype in function ExecInitResultTypeTL added hook ExecUpdateResultTypeTL_hook to set correct typmod of all attributes of result tuple.
    Extension PR: babelfish-for-postgresql/babelfish_extensions#3455
    
    Issues Resolved
    BABEL-5467

--- src/include/nodes/nodeFuncs.h
+++ src/include/nodes/nodeFuncs.h
@@ -14,6 +14,7 @@
 #define NODEFUNCS_H
 
 #include "nodes/parsenodes.h"
+#include "nodes/plannodes.h"
 
 struct PlanState;				/* avoid including execnodes.h too */
 
@@ -221,5 +222,8 @@ extern bool planstate_tree_walker_impl(struct PlanState *planstate,
 
 typedef int32 (*coalesce_typmod_hook_type) (const CoalesceExpr *cexpr);
 extern PGDLLEXPORT coalesce_typmod_hook_type coalesce_typmod_hook;
+
+typedef int32 (*exprTypmod_hook_type)(Plan *plan, Node *expr);
+extern PGDLLIMPORT exprTypmod_hook_type exprTypmod_hook;
 
 #endif							/* NODEFUNCS_H */
