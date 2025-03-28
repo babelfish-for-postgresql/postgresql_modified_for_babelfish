bce4062464e4212055a44eded7da0d016e184522
Peter Eisentraut <peter@eisentraut.org>
2025-03-28 10:49:15 +0100

    Modernize some code a bit
    Modernize code in ExecRelCheck() and ExecConstraints() a bit,
    preparing the way for some new code.
    
    Co-authored-by: jian he <jian.universality@gmail.com>
    Reviewed-by: Xuneng Zhou <xunengzhou@gmail.com>
    Reviewed-by: Navneet Kumar <thanit3111@gmail.com>
    Reviewed-by: Álvaro Herrera <alvherre@alvh.no-ip.org>
    Discussion: https://postgr.es/m/CACJufxHArQysbDkWFmvK+D1TPHQWWTxWN15cMuUaTYX3xhQXgg@mail.gmail.com
    (cherry picked from commit 747ddd38cbf6d32bca496e69c1efb2ae4fe333cc)

--- src/backend/executor/execMain.c
+++ src/backend/executor/execMain.c
@@ -1877,7 +1877,6 @@ ExecRelCheck(ResultRelInfo *resultRelInfo,
 	ConstrCheck *check = rel->rd_att->constr->check;
 	ExprContext *econtext;
 	MemoryContext oldContext;
-	int			i;
 
 	/*
 	 * CheckConstraintFetch let this pass with only a warning, but now we
@@ -1896,9 +1895,8 @@ ExecRelCheck(ResultRelInfo *resultRelInfo,
 	if (resultRelInfo->ri_CheckConstraintExprs == NULL)
 	{
 		oldContext = MemoryContextSwitchTo(estate->es_query_cxt);
-		resultRelInfo->ri_CheckConstraintExprs =
-			(ExprState **) palloc0(ncheck * sizeof(ExprState *));
-		for (i = 0; i < ncheck; i++)
+		resultRelInfo->ri_CheckConstraintExprs = palloc0_array(ExprState *, ncheck);
+		for (int i = 0; i < ncheck; i++)
 		{
 			Expr	   *checkconstr;
 
@@ -1924,7 +1922,7 @@ ExecRelCheck(ResultRelInfo *resultRelInfo,
 	econtext->ecxt_scantuple = slot;
 
 	/* And evaluate the constraints */
-	for (i = 0; i < ncheck; i++)
+	for (int i = 0; i < ncheck; i++)
 	{
 		ExprState  *checkconstr = resultRelInfo->ri_CheckConstraintExprs[i];
 
@@ -2083,16 +2081,16 @@ ExecConstraints(ResultRelInfo *resultRelInfo,
 
 	Assert(constr);				/* we should not be called otherwise */
 
+	/*
+	 * Verify not-null constraints.
+	 */
 	if (constr->has_not_null)
 	{
-		int			natts = tupdesc->natts;
-		int			attrChk;
-
-		for (attrChk = 1; attrChk <= natts; attrChk++)
+		for (AttrNumber attnum = 1; attnum <= tupdesc->natts; attnum++)
 		{
-			Form_pg_attribute att = TupleDescAttr(tupdesc, attrChk - 1);
+			Form_pg_attribute att = TupleDescAttr(tupdesc, attnum - 1);
 
-			if (att->attnotnull && slot_attisnull(slot, attrChk))
+			if (att->attnotnull && slot_attisnull(slot, attnum))
 			{
 				char	   *val_desc;
 				Relation	orig_rel = rel;
@@ -2137,16 +2135,19 @@ ExecConstraints(ResultRelInfo *resultRelInfo,
 														 64);
 
 				ereport(ERROR,
-						(errcode(ERRCODE_NOT_NULL_VIOLATION),
-						 errmsg("null value in column \"%s\" of relation \"%s\" violates not-null constraint",
-								NameStr(att->attname),
-								RelationGetRelationName(orig_rel)),
-						 val_desc ? errdetail("Failing row contains %s.", val_desc) : 0,
-						 errtablecol(orig_rel, attrChk)));
+						errcode(ERRCODE_NOT_NULL_VIOLATION),
+						errmsg("null value in column \"%s\" of relation \"%s\" violates not-null constraint",
+							   NameStr(att->attname),
+							   RelationGetRelationName(orig_rel)),
+						val_desc ? errdetail("Failing row contains %s.", val_desc) : 0,
+						errtablecol(orig_rel, attnum));
 			}
 		}
 	}
 
+	/*
+	 * Verify check constraints.
+	 */
 	if (rel->rd_rel->relchecks > 0)
 	{
 		const char *failed;
