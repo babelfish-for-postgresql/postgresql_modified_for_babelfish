e55f5a7568a3026e42be0a2c86f2cbce80e28dc3
Dean Rasheed <dean.a.rasheed@gmail.com>
2025-03-29 09:58:40 +0000

    Fix MERGE with DO NOTHING actions into a partitioned table.
    ExecInitPartitionInfo() duplicates much of the logic in
    ExecInitMerge(), except that it failed to handle DO NOTHING
    actions. This would cause an "unknown action in MERGE WHEN clause"
    error if a MERGE with any DO NOTHING actions attempted to insert into
    a partition not already initialised by ExecInitModifyTable().
    
    Bug: #18871
    Reported-by: Alexander Lakhin <exclusion@gmail.com>
    Author: Tender Wang <tndrwang@gmail.com>
    Reviewed-by: Gurjeet Singh <gurjeet@singh.im>
    Discussion: https://postgr.es/m/18871-b44e3c96de3bd2e8%40postgresql.org
    Backpatch-through: 15
    (cherry picked from commit 8b6a0e2392b9fc3f9b821da705797bb54c982dc1)

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -3877,7 +3877,7 @@ ExecInitMerge(ModifyTableState *mtstate, EState *estate)
 				case CMD_NOTHING:
 					break;
 				default:
-					elog(ERROR, "unknown operation");
+					elog(ERROR, "unknown action in MERGE WHEN clause");
 					break;
 			}
 		}
