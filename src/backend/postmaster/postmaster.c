7f2023fe8534d8ec50cf606f3f2d6af2379e4e17
Andres Freund <andres@anarazel.de>
2025-03-29 14:45:42 -0400

    aio, bufmgr: Comment fixes/improvements
    Some of these comments have been wrong for a while (12f3867f5534), some I
    recently introduced (da7226993fd, 55b454d0e14). This includes an update to a
    comment in FlushBuffer(), which will be copied in a future commit.
    
    These changes seem big enough to be worth doing in separate commits.
    
    Suggested-by: Noah Misch <noah@leadboat.com>
    Discussion: https://postgr.es/m/20250319212530.80.nmisch@google.com
    (cherry picked from commit 08ccd56ac765496a152ba50cf0ae743c39396f52)

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -4576,7 +4576,7 @@ maybe_adjust_io_workers(void)
 			++io_worker_count;
 		}
 		else
-			break;				/* XXX try again soon? */
+			break;				/* try again next time */
 	}
 
 	/* Too many running? */
