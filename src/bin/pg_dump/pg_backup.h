477c61997b5defc7307f42da0f506d88e1618f7d
Andrew Dunstan <andrew@dunslane.net>
2025-04-04 10:04:35 -0400

    Move common pg_dump code related to connections to a new file
    ConnectDatabase is used by pg_dumpall, pg_restore and pg_dump so move
    common code to new file.
    
    new file name: connectdb.c
    
    Author:    Mahendra Singh Thalor <mahi6run@gmail.com>
    (cherry picked from commit c1da7281060d646f863e920a1aac3b9dbc997672)

--- src/bin/pg_dump/pg_backup.h
+++ src/bin/pg_dump/pg_backup.h
@@ -298,9 +298,9 @@ typedef void (*SetupWorkerPtrType) (Archive *AH);
  * Main archiver interface.
  */
 
-extern void ConnectDatabase(Archive *AHX,
-							const ConnParams *cparams,
-							bool isReconnect);
+extern void ConnectDatabaseAhx(Archive *AHX,
+							   const ConnParams *cparams,
+							   bool isReconnect);
 extern void DisconnectDatabase(Archive *AHX);
 extern PGconn *GetConnection(Archive *AHX);
 
