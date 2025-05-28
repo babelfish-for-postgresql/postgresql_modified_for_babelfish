7013e0cc1e0b0e79c180b6d6edee8874460050ac
Tanya Gupta <tanyagp@amazon.com>
2025-05-28 12:27:20 +0000

    removing redundant import
    Signed-off-by: Tanya Gupta <tanyagp@amazon.com>

--- src/backend/parser/parse_type.c
+++ src/backend/parser/parse_type.c
@@ -26,7 +26,6 @@
 #include "utils/builtins.h"
 #include "utils/lsyscache.h"
 #include "utils/syscache.h"
-#include "utils/guc.h"
 
 static int32 typenameTypeMod(ParseState *pstate, const TypeName *typeName,
 							 Type typ);
