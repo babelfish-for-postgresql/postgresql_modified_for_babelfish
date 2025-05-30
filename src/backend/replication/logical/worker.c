d0c38fbf00b1d1a8b1d4d13c07a4468bbf5c6d9a
Nathan Bossart <nathan@postgresql.org>
2025-05-30 15:17:28 -0500

    Ensure we have a snapshot when updating various system catalogs.
    A few places that access system catalogs don't set up an active
    snapshot before potentially accessing their TOAST tables.  To fix,
    push an active snapshot just before each section of code that might
    require accessing one of these TOAST tables, and pop it shortly
    afterwards.  While at it, this commit adds some rather strict
    assertions in an attempt to prevent such issues in the future.
    
    Commit 16bf24e0e4 recently removed pg_replication_origin's TOAST
    table in order to fix the same problem for that catalog.  On the
    back-branches, those bugs are left in place.  We cannot easily
    remove a catalog's TOAST table on released major versions, and only
    replication origins with extremely long names are affected.  Given
    the low severity of the issue, fixing older versions doesn't seem
    worth the trouble of significantly modifying the patch.
    
    Also, on v13 and v14, the aforementioned strict assertions have
    been omitted because commit 2776922201, which added
    HaveRegisteredOrActiveSnapshot(), was not back-patched.  While we
    could probably back-patch it now, I've opted against it because it
    seems unlikely that new TOAST snapshot issues will be introduced in
    the oldest supported versions.
    
    Reported-by: Alexander Lakhin <exclusion@gmail.com>
    Reviewed-by: Michael Paquier <michael@paquier.xyz>
    Discussion: https://postgr.es/m/18127-fe54b6a667f29658%40postgresql.org
    Discussion: https://postgr.es/m/18309-c0bf914950c46692%40postgresql.org
    Discussion: https://postgr.es/m/ZvMSUPOqUU-VNADN%40nathan
    Backpatch-through: 13
    (cherry picked from commit 24135398f1e18e7779fd5052af8399c9d6bb5ff7)

--- src/backend/replication/logical/worker.c
+++ src/backend/replication/logical/worker.c
@@ -4761,8 +4761,16 @@ ApplyWorkerMain(Datum main_arg)
 			walrcv_startstreaming(LogRepWorkerWalRcvConn, &options);
 
 			StartTransactionCommand();
+
+			/*
+			 * Updating pg_subscription might involve TOAST table access, so
+			 * ensure we have a valid snapshot.
+			 */
+			PushActiveSnapshot(GetTransactionSnapshot());
+
 			UpdateTwoPhaseState(MySubscription->oid, LOGICALREP_TWOPHASE_STATE_ENABLED);
 			MySubscription->twophasestate = LOGICALREP_TWOPHASE_STATE_ENABLED;
+			PopActiveSnapshot();
 			CommitTransactionCommand();
 		}
 		else
@@ -4815,7 +4823,15 @@ DisableSubscriptionAndExit(void)
 
 	/* Disable the subscription */
 	StartTransactionCommand();
+
+	/*
+	 * Updating pg_subscription might involve TOAST table access, so ensure we
+	 * have a valid snapshot.
+	 */
+	PushActiveSnapshot(GetTransactionSnapshot());
+
 	DisableSubscription(MySubscription->oid);
+	PopActiveSnapshot();
 	CommitTransactionCommand();
 
 	/* Ensure we remove no-longer-useful entry for worker's start time */
@@ -4920,6 +4936,12 @@ clear_subscription_skip_lsn(XLogRecPtr finish_lsn)
 	}
 
 	/*
+	 * Updating pg_subscription might involve TOAST table access, so ensure we
+	 * have a valid snapshot.
+	 */
+	PushActiveSnapshot(GetTransactionSnapshot());
+
+	/*
 	 * Protect subskiplsn of pg_subscription from being concurrently updated
 	 * while clearing it.
 	 */
@@ -4976,6 +4998,8 @@ clear_subscription_skip_lsn(XLogRecPtr finish_lsn)
 
 	heap_freetuple(tup);
 	table_close(rel, NoLock);
+
+	PopActiveSnapshot();
 
 	if (started_tx)
 		CommitTransactionCommand();
