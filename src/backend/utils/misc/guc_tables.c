efd42908d0daef1df6859c57b1282543ecf2e444
Amit Kapila <akapila@postgresql.org>
2025-07-11 09:53:34 +0530

    Fix the handling of two GUCs during upgrade.
    Previously, the check_hook functions for max_slot_wal_keep_size and
    idle_replication_slot_timeout would incorrectly raise an ERROR for values
    set in postgresql.conf during upgrade, even though those values were not
    actively used in the upgrade process.
    
    To prevent logical slot invalidation during upgrade, we used to set
    special values for these GUCs. Now, instead of relying on those values, we
    directly prevent WAL removal and logical slot invalidation caused by
    max_slot_wal_keep_size and idle_replication_slot_timeout.
    
    Note: PostgreSQL 17 does not include the idle_replication_slot_timeout
    GUC, so related changes were not backported.
    
    BUG #18979
    Reported-by: jorsol <jorsol@gmail.com>
    Author: Dilip Kumar <dilipbalaut@gmail.com>
    Reviewed by: vignesh C <vignesh21@gmail.com>
    Reviewed by: Alvaro Herrera <alvherre@alvh.no-ip.org>
    Backpatch-through: 17, where it was introduced
    Discussion: https://postgr.es/m/219561.1751826409@sss.pgh.pa.us
    Discussion: https://postgr.es/m/18979-a1b7fdbb7cd181c6@postgresql.org
    
    (cherry picked from commit 24f6c1bd41d0631a04cc956cc8cafa0b117ab625)
    (cherry picked from commit 64bfa7aa7678b27a36140c4df11d1b7a8b853b8f)

--- src/backend/utils/misc/guc_tables.c
+++ src/backend/utils/misc/guc_tables.c
@@ -2971,7 +2971,7 @@ struct config_int ConfigureNamesInt[] =
 		},
 		&max_slot_wal_keep_size_mb,
 		-1, -1, MAX_KILOBYTES,
-		check_max_slot_wal_keep_size, NULL, NULL
+		NULL, NULL, NULL
 	},
 
 	{
