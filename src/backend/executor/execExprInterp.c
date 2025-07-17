35dc4710728e6c16369527fed43de9dbfd1cf0c7
Amit Langote <amitlan@postgresql.org>
2025-07-17 14:30:11 +0900

    Remove duplicate line
    In 231b7d670b21, while copy-pasting some code into
    ExecEvalJsonCoercionFinish(), I (amitlan) accidentally introduced
    a duplicate line.  Remove it.
    
    Reported-by: Jian He <jian.universality@gmail.com>
    Discussion: https://postgr.es/m/CACJufxHcf=BpmRAJcjgfjOUfV76MwKnyz1x3ErXsWL26EAFmng@mail.gmail.com
    (cherry picked from commit 7c6a3d838bae1b8b8a5a3caff8213855b9009ca2)

--- src/backend/executor/execExprInterp.c
+++ src/backend/executor/execExprInterp.c
@@ -4692,7 +4692,6 @@ ExecEvalJsonCoercionFinish(ExprState *state, ExprEvalStep *op)
 		 * JsonBehavior expression.
 		 */
 		jsestate->escontext.error_occurred = false;
-		jsestate->escontext.error_occurred = false;
 		jsestate->escontext.details_wanted = true;
 	}
 }
