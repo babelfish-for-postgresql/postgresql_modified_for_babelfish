f4812bbad42d0df842a5f3b3e45ce736749e5f5f
Etsuro Fujita <efujita@postgresql.org>
2025-08-08 10:50:04 +0900

    Disallow collecting transition tuples from child foreign tables.
    Commit 9e6104c66 disallowed transition tables on foreign tables, but
    failed to account for cases where a foreign table is a child table of a
    partitioned/inherited table on which transition tables exist, leading to
    incorrect transition tuples collected from such foreign tables for
    queries on the parent table triggering transition capture.  This
    occurred not only for inherited UPDATE/DELETE but for partitioned INSERT
    later supported by commit 3d956d956, which should have handled it at
    least for the INSERT case, but didn't.
    
    To fix, modify ExecAR*Triggers to throw an error if the given relation
    is a foreign table requesting transition capture.  Also, this commit
    fixes make_modifytable so that in case of an inherited UPDATE/DELETE
    triggering transition capture, FDWs choose normal operations to modify
    child foreign tables, not DirectModify; which is needed because they
    would otherwise skip the calls to ExecAR*Triggers at execution, causing
    unexpected behavior.
    
    Author: Etsuro Fujita <etsuro.fujita@gmail.com>
    Reviewed-by: Amit Langote <amitlangote09@gmail.com>
    Discussion: https://postgr.es/m/CAPmGK14QJYikKzBDCe3jMbpGENnQ7popFmbEgm-XTNuk55oyHg%40mail.gmail.com
    Backpatch-through: 13
    (cherry picked from commit e94fc1a8ad0fdbe797cc4ae18d48919a0cb87240)

--- src/backend/commands/trigger.c
+++ src/backend/commands/trigger.c
@@ -2520,6 +2520,15 @@ ExecARInsertTriggers(EState *estate, ResultRelInfo *relinfo,
 {
 	TriggerDesc *trigdesc = relinfo->ri_TrigDesc;
 
+	if (relinfo->ri_FdwRoutine && transition_capture &&
+		transition_capture->tcs_insert_new_table)
+	{
+		Assert(relinfo->ri_RootResultRelInfo);
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("cannot collect transition tuples from child foreign tables")));
+	}
+
 	if ((trigdesc && trigdesc->trig_insert_after_row) ||
 		(transition_capture && transition_capture->tcs_insert_new_table))
 		AfterTriggerSaveEvent(estate, relinfo, TRIGGER_EVENT_INSERT,
@@ -2839,6 +2848,15 @@ ExecARDeleteTriggers(EState *estate, ResultRelInfo *relinfo,
 {
 	TriggerDesc *trigdesc = relinfo->ri_TrigDesc;
 
+	if (relinfo->ri_FdwRoutine && transition_capture &&
+		transition_capture->tcs_delete_old_table)
+	{
+		Assert(relinfo->ri_RootResultRelInfo);
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("cannot collect transition tuples from child foreign tables")));
+	}
+
 	if ((trigdesc && trigdesc->trig_delete_after_row) ||
 		(transition_capture && transition_capture->tcs_delete_old_table))
 	{
@@ -3142,6 +3160,16 @@ ExecARUpdateTriggers(EState *estate, ResultRelInfo *relinfo,
 					 TransitionCaptureState *transition_capture)
 {
 	TriggerDesc *trigdesc = relinfo->ri_TrigDesc;
+
+	if (relinfo->ri_FdwRoutine && transition_capture &&
+		(transition_capture->tcs_update_old_table ||
+		 transition_capture->tcs_update_new_table))
+	{
+		Assert(relinfo->ri_RootResultRelInfo);
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("cannot collect transition tuples from child foreign tables")));
+	}
 
 	if ((trigdesc && trigdesc->trig_update_after_row) ||
 		(transition_capture &&
