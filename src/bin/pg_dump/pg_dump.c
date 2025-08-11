f00b6f2f4d209b690cce796058f4ba36b1d8fc10
Nathan Bossart <nathan@postgresql.org>
2025-08-11 09:00:00 -0500

    Restrict psql meta-commands in plain-text dumps.
    BABELFISH-CONFLICT: see Postgres community repo for original commit
    
    A malicious server could inject psql meta-commands into plain-text
    dump output (i.e., scripts created with pg_dump --format=plain,
    pg_dumpall, or pg_restore --file) that are run at restore time on
    the machine running psql.  To fix, introduce a new "restricted"
    mode in psql that blocks all meta-commands (except for \unrestrict
    to exit the mode), and teach pg_dump, pg_dumpall, and pg_restore to
    use this mode in plain-text dumps.
    
    While at it, encourage users to only restore dumps generated from
    trusted servers or to inspect it beforehand, since restoring causes
    the destination to execute arbitrary code of the source superusers'
    choice.  However, the client running the dump and restore needn't
    trust the source or destination superusers.
    
    Reported-by: Martin Rakhmanov
    Reported-by: Matthieu Denais <litezeraw@gmail.com>
    Reported-by: RyotaK <ryotak.mail@gmail.com>
    Suggested-by: Tom Lane <tgl@sss.pgh.pa.us>
    Reviewed-by: Noah Misch <noah@leadboat.com>
    Reviewed-by: Michael Paquier <michael@paquier.xyz>
    Reviewed-by: Peter Eisentraut <peter@eisentraut.org>
    Security: CVE-2025-8714
    Backpatch-through: 13
    (cherry picked from commit 42404050685de4047a7b57b13f8ec4959666328a)

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -418,6 +418,7 @@ main(int argc, char **argv)
 		{"rows-per-insert", required_argument, NULL, 10},
 		{"include-foreign-data", required_argument, NULL, 11},
 		{"bbf-database-name", required_argument, NULL, 30},
+		{"restrict-key", required_argument, NULL, 25},
 
 		{NULL, 0, NULL, 0}
 	};
@@ -633,6 +634,10 @@ main(int argc, char **argv)
 				dopt.include_everything = false;
 				break;
 
+			case 25:
+				dopt.restrict_key = pg_strdup(optarg);
+				break;
+
 			default:
 				/* getopt_long already emitted a complaint */
 				pg_log_error_hint("Try \"%s --help\" for more information.", progname);
@@ -695,7 +700,21 @@ main(int argc, char **argv)
 
 	/* archiveFormat specific setup */
 	if (archiveFormat == archNull)
+	{
 		plainText = 1;
+
+		/*
+		 * If you don't provide a restrict key, one will be appointed for you.
+		 */
+		if (!dopt.restrict_key)
+			dopt.restrict_key = generate_restrict_key();
+		if (!dopt.restrict_key)
+			pg_fatal("could not generate restrict key");
+		if (!valid_restrict_key(dopt.restrict_key))
+			pg_fatal("invalid restrict key");
+	}
+	else if (dopt.restrict_key)
+		pg_fatal("option --restrict-key can only be used with --format=plain");
 
 	/* Custom and directory formats are compressed by default, others not */
 	if (compressLevel == -1)
@@ -962,6 +981,7 @@ main(int argc, char **argv)
 	ropt->sequence_data = dopt.sequence_data;
 	ropt->binary_upgrade = dopt.binary_upgrade;
 	ropt->babelfish_db = isBabelfishDatabase(fout);
+	ropt->restrict_key = dopt.restrict_key ? pg_strdup(dopt.restrict_key) : NULL;
 
 	if (compressLevel == -1)
 		ropt->compression = 0;
@@ -1059,6 +1079,7 @@ help(const char *progname)
 	printf(_("  --no-unlogged-table-data     do not dump unlogged table data\n"));
 	printf(_("  --on-conflict-do-nothing     add ON CONFLICT DO NOTHING to INSERT commands\n"));
 	printf(_("  --quote-all-identifiers      quote all identifiers, even if not key words\n"));
+	printf(_("  --restrict-key=RESTRICT_KEY  use provided string as psql \\restrict key\n"));
 	printf(_("  --rows-per-insert=NROWS      number of rows per INSERT; implies --inserts\n"));
 	printf(_("  --section=SECTION            dump named section (pre-data, data, or post-data)\n"));
 	printf(_("  --serializable-deferrable    wait until the dump can run without anomalies\n"));
