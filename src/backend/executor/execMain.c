c69538abc749bb07cb2dbf9e2e8b2e974ccf4245
Dean Rasheed <dean.a.rasheed@gmail.com>
2025-09-04 15:57:18 +0100

    Fix compiler error introduced by 5386bfb9c1f.
    Per buildfarm member wrasse, void function cannot return a value.
    This only affects v13-v17, where an ABI-compatible wrapper function
    was added.
    
    Backpatch-through: 13-17

--- src/backend/executor/execMain.c
+++ src/backend/executor/execMain.c
@@ -1152,7 +1152,7 @@ CheckValidResultRelNew(ResultRelInfo *resultRelInfo, CmdType operation,
 void
 CheckValidResultRel(ResultRelInfo *resultRelInfo, CmdType operation)
 {
-	return CheckValidResultRelNew(resultRelInfo, operation, ONCONFLICT_NONE);
+	CheckValidResultRelNew(resultRelInfo, operation, ONCONFLICT_NONE);
 }
 
 /*
