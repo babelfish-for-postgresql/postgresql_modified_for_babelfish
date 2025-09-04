c1130e8fe656dc96718d11a4c0ac8a26540ac39c
Dean Rasheed <dean.a.rasheed@gmail.com>
2025-09-04 11:34:25 +0100

    Fix replica identity check for INSERT ON CONFLICT DO UPDATE.
    If an INSERT has an ON CONFLICT DO UPDATE clause, the executor must
    check that the target relation supports UPDATE as well as INSERT. In
    particular, it must check that the target relation has a REPLICA
    IDENTITY if it publishes updates. Formerly, it was not doing this
    check, which could lead to silently breaking replication.
    
    Fix by adding such a check to CheckValidResultRel(), which requires
    adding a new onConflictAction argument. In back-branches, preserve ABI
    compatibility by introducing a wrapper function with the original
    signature.
    
    Author: Zhijie Hou <houzj.fnst@fujitsu.com>
    Reviewed-by: Ashutosh Bapat <ashutosh.bapat.oss@gmail.com>
    Reviewed-by: Dean Rasheed <dean.a.rasheed@gmail.com>
    Tested-by: Chao Li <li.evan.chao@gmail.com>
    Discussion: https://postgr.es/m/OS3PR01MB57180C87E43A679A730482DF94B62@OS3PR01MB5718.jpnprd01.prod.outlook.com
    Backpatch-through: 13

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -4419,7 +4419,8 @@ ExecInitModifyTable(ModifyTable *node, EState *estate, int eflags)
 		/*
 		 * Verify result relation is a valid target for the current operation
 		 */
-		CheckValidResultRel(resultRelInfo, operation);
+		CheckValidResultRelNew(resultRelInfo, operation,
+							   node->onConflictAction);
 
 		resultRelInfo++;
 		i++;
