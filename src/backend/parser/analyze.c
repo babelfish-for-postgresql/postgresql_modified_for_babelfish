1d73cd8c3667e026915b933a226ca4b3002f8c0c
Herambh Shah <herambhs@amazon.com>
2025-09-10 16:44:38 +0000

    temporarily disabling percent hook to test failing test case
    Signed-off-by: Herambh Shah <herambhs@amazon.com>

--- src/backend/parser/analyze.c
+++ src/backend/parser/analyze.c
@@ -1445,7 +1445,7 @@ transformSelectStmt(ParseState *pstate, SelectStmt *stmt)
 	qry->commandType = CMD_SELECT;
 
 	/* Transform query if percent Operator is present with Top Clause */
-	if(transform_percent_clause_hook)
+	if(transform_percent_clause_hook && false)
 	{
 		(*transform_percent_clause_hook)(pstate, stmt);
 	}
