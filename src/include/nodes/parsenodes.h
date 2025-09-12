9d384a4779b0a054fc3df810b1133c23213ba68a
Herambh Shah <herambhs@amazon.com>
2025-09-12 07:14:05 +0000

    Removed topclause node
    Signed-off-by: Herambh Shah <herambhs@amazon.com>

--- src/include/nodes/parsenodes.h
+++ src/include/nodes/parsenodes.h
@@ -2185,18 +2185,6 @@ typedef struct SelectStmt
 	bool isPercent;
 } SelectStmt;
 
-/*
- * This node is used in gram-tsql-rule.y to store limitCount and Percent op information.
- * Furthuremore, through this node, we are updating limitCount and isPercent( Added for supporing tsql Percent Op) fields
- * in SelectStmt, UpdateStmt, DeleteStmt, InsertStmt
- */
-typedef struct TopClause
-{
-    NodeTag   type;           /* type tag */
-    Node     *limitCount;     /* expression for the limit count/percent */
-    bool      isPercent;      /* true if PERCENT specified */
-} TopClause;
-
 
 /*
  * This node is used in gram-tsql-rule.y to store limitCount and Percent op information.
