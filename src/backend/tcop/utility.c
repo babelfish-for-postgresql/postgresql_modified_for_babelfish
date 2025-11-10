acaa0214fe0e9ef6ae3ab3f65c52f874553bd642
Nathan Bossart <nathan@postgresql.org>
2025-11-10 09:00:00 -0600

    Check for CREATE privilege on the schema in CREATE STATISTICS.
    This omission allowed table owners to create statistics in any
    schema, potentially leading to unexpected naming conflicts.  For
    ALTER TABLE commands that require re-creating statistics objects,
    skip this check in case the user has since lost CREATE on the
    schema.  The addition of a second parameter to CreateStatistics()
    breaks ABI compatibility, but we are unaware of any impacted
    third-party code.
    
    Reported-by: Jelte Fennema-Nio <postgres@jeltef.nl>
    Author: Jelte Fennema-Nio <postgres@jeltef.nl>
    Co-authored-by: Nathan Bossart <nathandbossart@gmail.com>
    Reviewed-by: Noah Misch <noah@leadboat.com>
    Reviewed-by: Álvaro Herrera <alvherre@kurilemu.de>
    Security: CVE-2025-12817
    Backpatch-through: 13
    (cherry picked from commit 00eb646ea43410e5df77fed96f4a981e66811796)

--- src/backend/tcop/utility.c
+++ src/backend/tcop/utility.c
@@ -1924,7 +1924,7 @@ ProcessUtilitySlow(ParseState *pstate,
 					/* Run parse analysis ... */
 					stmt = transformStatsStmt(relid, stmt, queryString);
 
-					address = CreateStatistics(stmt);
+					address = CreateStatistics(stmt, true);
 				}
 				break;
 
