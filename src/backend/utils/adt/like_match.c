37328d9cf80cbfb8b1ffa04d937bf85cf8e8a850
Thomas Munro <tmunro@postgresql.org>
2026-01-07 22:14:31 +1300

    Replace pg_mblen() with bounds-checked versions.
    A corrupted string could cause code that iterates with pg_mblen() to
    overrun its buffer.  Fix, by converting all callers to one of the
    following:
    
    1. Callers with a null-terminated string now use pg_mblen_cstr(), which
    raises an "illegal byte sequence" error if it finds a terminator in the
    middle of the sequence.
    
    2. Callers with a length or end pointer now use either
    pg_mblen_with_len() or pg_mblen_range(), for the same effect, depending
    on which of the two seems more convenient at each site.
    
    3. A small number of cases pre-validate a string, and can use
    pg_mblen_unbounded().
    
    The traditional pg_mblen() function and COPYCHAR macro still exist for
    backward compatibility, but are no longer used by core code and are
    hereby deprecated.  The same applies to the t_isXXX() functions.
    
    Security: CVE-2026-2006
    Backpatch-through: 14
    Co-authored-by: Thomas Munro <thomas.munro@gmail.com>
    Co-authored-by: Noah Misch <noah@leadboat.com>
    Reviewed-by: Heikki Linnakangas <hlinnaka@iki.fi>
    Reported-by: Paul Gerste (as part of zeroday.cloud)
    Reported-by: Moritz Sanft (as part of zeroday.cloud)
    (cherry picked from commit 7b5fc85bef8a3baa530ec98f89376f9d4b7de83c)

--- src/backend/utils/adt/like_match.c
+++ src/backend/utils/adt/like_match.c
@@ -558,6 +558,7 @@ do_like_escape(text *pat, text *esc)
 					 errhint("Escape string must be empty or one character.")));
 
 		e = VARDATA_ANY(esc);
+		elen = VARSIZE_ANY_EXHDR(esc);
 
 		if (sql_dialect == SQL_DIALECT_TSQL)
 		{
@@ -567,7 +568,7 @@ do_like_escape(text *pat, text *esc)
 			afterescape = false;
 			while (plen > 0)
 			{
-				if (CHAREQ(p, e) && !afterescape)
+				if (CHAREQ(p, plen, e, elen) && !afterescape)
 				{
 					/* check if direct copy string (unicode char) is valid */
 					
@@ -603,7 +604,7 @@ do_like_escape(text *pat, text *esc)
 			afterescape = false;
 			while (plen > 0)
 			{
-				if (CHAREQ(p, e) && !afterescape)
+				if (CHAREQ(p, plen, e, elen) && !afterescape)
 				{
 					*r++ = '\\';
 					NextChar(p, plen);
