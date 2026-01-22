8393156e3d434e828e93ebce721f96ffd4c86ba2
Tom Lane <tgl@sss.pgh.pa.us>
2026-01-22 18:35:31 -0500

    Remove faulty Assert in partitioned INSERT...ON CONFLICT DO UPDATE.
    Commit f16241bef mistakenly supposed that INSERT...ON CONFLICT DO
    UPDATE rejects partitioned target tables.  (This may have been
    accurate when the patch was written, but it was already obsolete
    when committed.)  Hence, there's an assertion that we can't see
    ItemPointerIndicatesMovedPartitions() in that path, but the assertion
    is triggerable.
    
    Some other places throw error if they see a moved-across-partitions
    tuple, but there seems no need for that here, because if we just retry
    then we get the same behavior as in the update-within-partition case,
    as demonstrated by the new isolation test.  So fix by deleting the
    faulty Assert.  (The fact that this is the fix doubtless explains
    why we've heard no field complaints: the behavior of a non-assert
    build is fine.)
    
    The TM_Deleted case contains a cargo-culted copy of the same Assert,
    which I also deleted to avoid confusion, although I believe that one
    is actually not triggerable.
    
    Per our code coverage report, neither the TM_Updated nor the
    TM_Deleted case were reached at all by existing tests, so this
    patch adds tests for both.
    
    Reported-by: Dmitry Koval <d.koval@postgrespro.ru>
    Author: Joseph Koshakow <koshy44@gmail.com>
    Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/f5fffe4b-11b2-4557-a864-3587ff9b4c36@postgrespro.ru
    Backpatch-through: 14
    (cherry picked from commit 55b5859d41e3ee78aeb09249ec395062cc17f8f4)

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -2285,14 +2285,6 @@ ExecOnConflictUpdate(ModifyTableState *mtstate,
 						 errmsg("could not serialize access due to concurrent update")));
 
 			/*
-			 * As long as we don't support an UPDATE of INSERT ON CONFLICT for
-			 * a partitioned table we shouldn't reach to a case where tuple to
-			 * be lock is moved to another partition due to concurrent update
-			 * of the partition key.
-			 */
-			Assert(!ItemPointerIndicatesMovedPartitions(&tmfd.ctid));
-
-			/*
 			 * Tell caller to try again from the very start.
 			 *
 			 * It does not make sense to use the usual EvalPlanQual() style
@@ -2309,7 +2301,6 @@ ExecOnConflictUpdate(ModifyTableState *mtstate,
 						 errmsg("could not serialize access due to concurrent delete")));
 
 			/* see TM_Updated case */
-			Assert(!ItemPointerIndicatesMovedPartitions(&tmfd.ctid));
 			ExecClearTuple(existing);
 			return false;
 
