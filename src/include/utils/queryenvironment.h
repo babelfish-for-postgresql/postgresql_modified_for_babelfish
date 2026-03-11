181e2bb73e2a4935aff1c0baa614c679f7e2c6fb
Ayush-061 <ayushhx@amazon.com>
2026-03-11 10:15:33 +0000

    Function moved to c file, for consistency

--- src/include/utils/queryenvironment.h
+++ src/include/utils/queryenvironment.h
@@ -157,6 +157,8 @@ extern bool ENRGetSystableScan(Relation rel, Oid indexoid, int nkeys, ScanKey ke
 extern bool ENRAddTuple(Relation rel, HeapTuple tup);
 extern bool ENRDropTuple(Relation rel, HeapTuple tup);
 extern bool ENRUpdateTuple(Relation rel, HeapTuple tup);
+extern Oid get_toast_parent_oid(const char *relname);
+extern QueryEnvironment *find_ENR_queryEnv(Oid relid);
 
 extern void ENRDropEntry(Oid id);
 extern PGDLLEXPORT void ENRDropTempTables(QueryEnvironment *queryEnv);
@@ -197,42 +199,5 @@ extern PGDLLIMPORT is_enr_to_sys_object_dependency_hook_type is_enr_to_sys_objec
 #define BBF_TEMP_TOAST_PREFIX		"#pg_toast_"
 #define BBF_TABLEVAR_TOAST_PREFIX	"@pg_toast_"
 #define BBF_TOAST_PREFIX_LEN		10
-
-/*
- * Extract parent table OID from temp toast table name.
- * Toast table names follow the pattern "#pg_toast_<parent_rel_oid>" for temp tables
- * or "@pg_toast_<parent_rel_oid>" for table variables.
- * Returns the parent OID, or InvalidOid if the name doesn't match either pattern.
- */
-static inline Oid
-get_toast_parent_oid(const char *relname)
-{
-	if (strncmp(relname, BBF_TEMP_TOAST_PREFIX, BBF_TOAST_PREFIX_LEN) == 0 ||
-		strncmp(relname, BBF_TABLEVAR_TOAST_PREFIX, BBF_TOAST_PREFIX_LEN) == 0)
-		return (Oid) strtoul(relname + BBF_TOAST_PREFIX_LEN, NULL, 10);
-	return InvalidOid;
-}
-
-/*
- * find_ENR_queryEnv
- *
- * Find the query environment where an ENR with the given OID is registered.
- * Searches from currentQueryEnv up through parent environments.
- * Returns the queryEnv where the ENR is found, or NULL if not found.
- */
-static inline QueryEnvironment *
-find_ENR_queryEnv(Oid relid)
-{
-	QueryEnvironment *qe = currentQueryEnv;
-
-	while (qe)
-	{
-		if (get_ENR_withoid(qe, relid, ENR_TSQL_TEMP, false))
-			return qe;
-		qe = qe->parentEnv;
-	}
-
-	return NULL;
-}
 
 #endif							/* QUERYENVIRONMENT_H */
