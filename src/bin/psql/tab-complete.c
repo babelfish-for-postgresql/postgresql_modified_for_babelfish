001b473dc251d83824239ba4bffc0b792d4370e1
Fujii Masao <fujii@postgresql.org>
2026-07-03 13:46:35 +0900

    psql: Fix \df tab completion for procedures
    Commit fb421231daa extended \df to include procedures, but its tab
    completion continued not to show procedures.
    
    Update \df tab completion to include procedures as well.
    
    Backpatch to all supported versions.
    
    Author: Erik Wienhold <ewie@ewie.name>
    Reviewed-by: Surya Poondla <suryapoondla4@gmail.com>
    Reviewed-by: Fujii Masao <masao.fujii@gmail.com>
    Discussion: https://postgr.es/m/10fbfdfe-80f6-4ef9-b8b3-f7be0eb53a50@ewie.name
    Backpatch-through: 14
    (cherry picked from commit c25737c8966329bbc1909de97f166f83e696eff5)
    Signed-off-by: Rucha Kulkarni <ruchask@amazon.com>

--- src/bin/psql/tab-complete.c
+++ src/bin/psql/tab-complete.c
@@ -4583,7 +4583,7 @@ psql_completion(const char *text, int start, int end)
 	else if (TailMatchesCS("\\dew*"))
 		COMPLETE_WITH_QUERY(Query_for_list_of_fdws);
 	else if (TailMatchesCS("\\df*"))
-		COMPLETE_WITH_VERSIONED_SCHEMA_QUERY(Query_for_list_of_functions);
+		COMPLETE_WITH_SCHEMA_QUERY(Query_for_list_of_routines);
 	else if (HeadMatchesCS("\\df*"))
 		COMPLETE_WITH_SCHEMA_QUERY(Query_for_list_of_datatypes);
 
