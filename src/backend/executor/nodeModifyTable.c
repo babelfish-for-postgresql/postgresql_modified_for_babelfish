398ee325fd1438cebd1092970f27d5e4dda6a683
Dean Rasheed <dean.a.rasheed@gmail.com>
2026-07-08 20:46:26 +0100

    Fix RETURNING OLD with BEFORE UPDATE trigger and concurrent update.
    When executing an UPDATE with a RETURNING clause on a table with a
    BEFORE UPDATE row trigger, the computation of the OLD values in the
    RETURNING list was incorrect if the target tuple was concurrently
    updated by another session, at isolation level READ COMMITTED.
    
    The problem was that the trigger code would lock the target tuple,
    waiting for the other session to commit, and then fetch the updated
    target tuple, but ExecUpdate() would not realise that the target tuple
    had changed, and use the outdated target tuple for computing OLD
    values. Fix by having ExecUpdate() check the TM_FailureData from
    trigger execution and re-fetch the target tuple if necessary.
    
    Re-fetching the target tuple like this is a little inefficient, but
    probably negligible compared to the trigger execution and update. A
    better long-term fix might be to move the EPQ code out of trigger.c,
    and let ExecUpdate() handle it, like ExecMergeMatched() does, but that
    would likely mean changing the trigger API, which seems a bit much for
    back-patching.
    
    Backpatch to v18, where support for RETURNING OLD/NEW was added.
    
    Bug: #19536
    Reported-by: Jonas Boberg <bobergj@gmail.com>
    Diagnosed-by: Bharath Rupireddy <bharath.rupireddyforpostgres@gmail.com>
    Author: Dean Rasheed <dean.a.rasheed@gmail.com>
    Reviewed-by: Bharath Rupireddy <bharath.rupireddyforpostgres@gmail.com>
    Discussion: https://postgr.es/m/19536-73ce5847e6c0e7b1@postgresql.org
    Backpatch-through: 18
    (cherry picked from commit 4908225bebdc31c9b8f735ad1db3d08d9bc85148)
    Signed-off-by: Rucha Kulkarni <ruchask@amazon.com>

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -2503,8 +2503,27 @@ ExecUpdate(ModifyTableContext *context, ResultRelInfo *resultRelInfo,
 	 * Prepare for the update.  This includes BEFORE ROW triggers, so we're
 	 * done if it says we are.
 	 */
+	context->tmfd.traversed = false;
 	if (!ExecUpdatePrologue(context, resultRelInfo, tupleid, oldtuple, slot, NULL))
 		return NULL;
+
+	/*
+	 * If the target tuple was concurrently updated, the trigger code will
+	 * have done EPQ and updated tupleid, following the update chain.  In this
+	 * case, we must fetch the most recent version of old tuple for the
+	 * benefit of RETURNING.  Technically, we could get away with not doing
+	 * this, if there is no RETURNING clause, or it doesn't refer to OLD, but
+	 * it seems preferable to always ensure that the contents of oldSlot are
+	 * correct.
+	 */
+	if (context->tmfd.traversed)
+	{
+		if (!table_tuple_fetch_row_version(resultRelInfo->ri_RelationDesc,
+										   tupleid,
+										   SnapshotAny,
+										   oldSlot))
+			elog(ERROR, "failed to re-fetch tuple updated during trigger execution");
+	}
 
 	if (resultRelInfo->ri_TrigDesc &&
 		resultRelInfo->ri_TrigDesc->trig_update_instead_statement &&
