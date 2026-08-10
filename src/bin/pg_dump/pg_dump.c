348bbb4f0b1ea5259d81ff8bd84fc849f977f8c1
Tom Lane <tgl@sss.pgh.pa.us>
2026-08-10 06:38:35 -0700

    pg_dump: avoid assuming how long pg_proc.protrftypes can be.
    The backend doesn't impose any particular limit on the length of this
    array, and since there could be entries for both input and output
    arguments, it's feasible for the length to exceed FUNC_MAX_ARGS
    even without funny business.  This could lead to crashes or worse.
    
    Moreover, pg_dump shouldn't rely on hard-coding FUNC_MAX_ARGS in the
    first place: it has no business assuming that the backend it's dumping
    from was compiled with the same value of FUNC_MAX_ARGS that it is.
    So the stanza in dumpFunc() that allocates exactly FUNC_MAX_ARGS space
    for the parsed OID array is fundamentally misguided.  And it's broken
    in another way too: if there are exactly FUNC_MAX_ARGS OIDs, then
    parseOidArray won't zero-fill any entries, allowing the subsequent
    loop to run off the end of the array.  A crash seems unlikely in
    this variant, but garbage output is certain.
    
    To fix, redesign parseOidArray's API so that it does the
    array-mallocing, which simplifies the callers anyway.  While we're
    here, tighten and modernize it a bit; in particular, split it into
    separate functions for OIDs and integers, as was foreseen long ago.
    This lets us get rid of the confusing type-punning involved in
    having IndxInfo.indkeys be declared as "Oid *" when it's really
    potentially-signed ints.  Also, most of the callers expect an exact
    number of array entries, so make it verify that not just check for
    "too many".
    
    I noted while testing that this dumpFunc() stanza isn't even reached
    during check-world.  Add a function with transform to the regression
    tests to rectify that.
    
    Reported-by: Masahiko Sawada <sawada.mshk@gmail.com>
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Reviewed-by: Masahiko Sawada <sawada.mshk@gmail.com>
    Backpatch-through: 14
    Security: CVE-2026-19385
    (cherry picked from commit 57aa21f69127d21e284c5db8eb488b1eccbb6a1f)
    Signed-off-by: Rucha Kulkarni <ruchask@amazon.com>

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -6209,12 +6209,8 @@ getAggregates(Archive *fout, int *numAggs)
 		if (agginfo[i].aggfn.nargs == 0)
 			agginfo[i].aggfn.argtypes = NULL;
 		else
-		{
-			agginfo[i].aggfn.argtypes = (Oid *) pg_malloc(agginfo[i].aggfn.nargs * sizeof(Oid));
-			parseOidArray(PQgetvalue(res, i, i_proargtypes),
-						  agginfo[i].aggfn.argtypes,
-						  agginfo[i].aggfn.nargs);
-		}
+			agginfo[i].aggfn.argtypes = parseOidArray(PQgetvalue(res, i, i_proargtypes),
+													  agginfo[i].aggfn.nargs);
 		agginfo[i].aggfn.postponed_def = false; /* might get set during sort */
 
 		/* Decide whether we want to dump it */
@@ -6441,11 +6437,8 @@ getFuncs(Archive *fout, int *numFuncs)
 		if (finfo[i].nargs == 0)
 			finfo[i].argtypes = NULL;
 		else
-		{
-			finfo[i].argtypes = (Oid *) pg_malloc(finfo[i].nargs * sizeof(Oid));
-			parseOidArray(PQgetvalue(res, i, i_proargtypes),
-						  finfo[i].argtypes, finfo[i].nargs);
-		}
+			finfo[i].argtypes = parseOidArray(PQgetvalue(res, i, i_proargtypes),
+											  finfo[i].nargs);
 		finfo[i].postponed_def = false; /* might get set during sort */
 
 		/* Decide whether we want to dump it */
@@ -7752,9 +7745,8 @@ getIndexes(Archive *fout, TableInfo tblinfo[], int numTables)
 			indxinfo[j].indreloptions = pg_strdup(PQgetvalue(res, j, i_indreloptions));
 			indxinfo[j].indstatcols = pg_strdup(PQgetvalue(res, j, i_indstatcols));
 			indxinfo[j].indstatvals = pg_strdup(PQgetvalue(res, j, i_indstatvals));
-			indxinfo[j].indkeys = (Oid *) pg_malloc(indxinfo[j].indnattrs * sizeof(Oid));
-			parseOidArray(PQgetvalue(res, j, i_indkey),
-						  indxinfo[j].indkeys, indxinfo[j].indnattrs);
+			indxinfo[j].indkeys = parseIntArray(PQgetvalue(res, j, i_indkey),
+												indxinfo[j].indnattrs);
 			indxinfo[j].indisclustered = (PQgetvalue(res, j, i_indisclustered)[0] == 't');
 			indxinfo[j].indisreplident = (PQgetvalue(res, j, i_indisreplident)[0] == 't');
 			indxinfo[j].parentidx = atooid(PQgetvalue(res, j, i_parentidx));
@@ -12730,12 +12722,10 @@ dumpFunc(Archive *fout, const FuncInfo *finfo)
 
 	if (protrftypes != NULL && strcmp(protrftypes, "") != 0)
 	{
-		Oid		   *typeids = palloc(FUNC_MAX_ARGS * sizeof(Oid));
-		int			i;
+		Oid		   *typeids = parseOidArray(protrftypes, -1);
 
 		appendPQExpBufferStr(q, " TRANSFORM ");
-		parseOidArray(protrftypes, typeids, FUNC_MAX_ARGS);
-		for (i = 0; typeids[i]; i++)
+		for (int i = 0; typeids[i]; i++)
 		{
 			if (i != 0)
 				appendPQExpBufferStr(q, ", ");
@@ -17423,7 +17413,7 @@ dumpConstraint(Archive *fout, const ConstraintInfo *coninfo)
 							  coninfo->contype == 'p' ? "PRIMARY KEY" : "UNIQUE");
 			for (k = 0; k < indxinfo->indnkeyattrs; k++)
 			{
-				int			indkey = (int) indxinfo->indkeys[k];
+				int			indkey = indxinfo->indkeys[k];
 				const char *attname;
 
 				if (indkey == InvalidAttrNumber)
@@ -17440,7 +17430,7 @@ dumpConstraint(Archive *fout, const ConstraintInfo *coninfo)
 
 			for (k = indxinfo->indnkeyattrs; k < indxinfo->indnattrs; k++)
 			{
-				int			indkey = (int) indxinfo->indkeys[k];
+				int			indkey = indxinfo->indkeys[k];
 				const char *attname;
 
 				if (indkey == InvalidAttrNumber)
